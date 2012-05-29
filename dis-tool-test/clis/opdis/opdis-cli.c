4e814a60102743e601a1a6917bab6964b909801f
Julian Kranz <julian@juliankranz.de>
2012-05-29 11:00:52 +0200

    X86 Disassembler tools
    - Continued to implement: CLI tool for opdis

--- dis-tool-test/clis/opdis/opdis-cli.c
+++ dis-tool-test/clis/opdis/opdis-cli.c
@@ -8,6 +8,7 @@
 #include <opdis/opdis.h>
 #include <opdis/types.h>
 #include <bfd.h>
+#include <readhex.h>
 
 void disassemble_mem(void * src, size_t length) {
 	opdis_t o;
@@ -18,16 +19,18 @@ void disassemble_mem(void * src, size_t length) {
 	o = opdis_init();
 
 	//opdis_set_arch(o, bfd_arch_k1om, bfd_mach_k1om, NULL);
-	const bfd_arch_info_type *arch_info = bfd_scan_arch("i386:x86-64");
-	opdis_set_arch(o, arch_info->arch, arch_info->mach, NULL );
+	//const bfd_arch_info_type *arch_info = bfd_scan_arch("i386:x86-64");
+	opdis_set_arch(o, bfd_arch_i386, bfd_mach_x86_64, print_insn_i386_att);
 
 	opdis_disasm_linear(o, buf, (opdis_vma_t)src, length);
 	opdis_term(o);
 }
 
-int main() {
-	char x[] = { 0x48, 0x8b, 0x05, 0xf9, 0x27, 0x20, 0x00 };
-	disassemble_mem(x, 7);
+int main(int argc, char **argv) {
+	char *buffer;
+	size_t length = readhex_hex_read(stdin, &buffer);
+
+	disassemble_mem(buffer, length);
 
 	return 0;
 }
