513f99ab61481bb836befccf3ee240aadf7b9210
mb0 <unknown>
2012-06-04 06:54:04 +0200

    Added linear sweep using beaengine.

--- toys/sweep/sweep-beaengine.c
+++ toys/sweep/sweep-beaengine.c
@@ -1,61 +1,51 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <bfd.h>
-#include <xed-interface.h>
+#include <beaengine/BeaEngine.h>
 
 int main (int argc, char** argv) {
   if (argc < 2)
     exit(1);
   const char* fn=argv[1];
-
   fprintf(stderr,"file is %s\n",fn);
-
   bfd_init();
   bfd* bfd = bfd_openr(fn, NULL);
   if (bfd==NULL)
     exit(1);
-
   if (!bfd_check_format(bfd,bfd_object))
     exit(1);
-
   asection* s = bfd_get_section_by_name(bfd,".text");
   if (s==NULL)
     exit(1);
-
   unsigned char* blob;
   bfd_size_type sz = s->size;
   fprintf(stderr,".text is %zu bytes\n",sz);
-
   if(!bfd_malloc_and_get_section(bfd,s,&blob))
     exit(1);
 
-  xed_state_t state;
-  xed_decoded_inst_t insnObj;
-  xed_decoded_inst_t* insn = &insnObj;
-  xed_tables_init();
-  xed_state_zero(&state);
-  state.mmode = XED_MACHINE_MODE_LONG_64;
-  state.stack_addr_width = XED_ADDRESS_WIDTH_32b;
-  xed_decoded_inst_zero_set_mode(insn, &state);
-  xed_decoded_inst_set_input_chip(insn, XED_CHIP_INVALID);
-  char insnstr[128];
+  DISASM disObj;
+  DISASM* dis = &disObj;
+  memset(dis,0,sizeof(DISASM));
+
   unsigned int len;
   unsigned char* blobb = blob;
   unsigned int invalid = 0;
   unsigned int n = 0;
-  xed_error_enum_t r;
+  dis->Archi = 64;
   do {
-    xed_decoded_inst_zero_set_mode(insn, &state);
-    xed_decoded_inst_set_input_chip(insn, XED_CHIP_INVALID);
-    r = xed_decode(insn, blobb, sz);
-    if (r==XED_ERROR_NONE) {
-       len = xed_decoded_inst_get_length(insn);
-       //xed_decoded_inst_dump_intel_format(insn,insnstr,128,0);
-       //printf("%-27s\n",insnstr);
+    dis->EIP = (UIntPtr)blobb;
+    dis->SecurityBlock = sz; 
+    len = Disasm(dis);
+    if (len==OUT_OF_BLOCK) {
+      fprintf(stderr,"out-of-block-error");
+      exit(1);
+    } else if (len==UNKNOWN_OPCODE) {
+      invalid++;
+      len = 1;
     } else {
-       invalid++;
-       len = 1;
+      puts(dis->CompleteInstr);
     }
     blobb += len;
     sz-=len;
