fadb5f222b268e33ef921432c806188e7d3eee0f
Julian Kranz <julian@juliankranz.de>
2012-06-25 13:31:22 +0200

    AVR Example Specification
    - Bug fixes

--- examples/avr.d
+++ examples/avr.d
@@ -2,11 +2,7 @@ granularity = 8
 export = decode
 
 val decode = do
- update@{
-  rd='',
-  rr='',
-  k=''
- };
+ update@{rd='',rr='',ck=''};
  /
 end
 
@@ -15,7 +11,7 @@ datatype operand =
  | REGHL of {regh:register,regl:register}
  | IMM6 of 6
 
-type binop = {left:operand,right:operand}
+type binop = {first:operand,second:operand}
 
 datatype instruction =
    ADC of binop
@@ -57,52 +53,56 @@ datatype register =
 
 val register-from-bits bits =
  case bits of
-    '00000':REG R0
-  | '00001':REG R1
-  | '00010':REG R2
-  | '00011':REG R3
-  | '00100':REG R4
-  | '00101':REG R5
-  | '00110':REG R6
-  | '00111':REG R7
-  | '01000':REG R8
-  | '01001':REG R9
-  | '01010':REG R10
-  | '01011':REG R11
-  | '01100':REG R12
-  | '01101':REG R13
-  | '01110':REG R14
-  | '01111':REG R15
-  | '10000':REG R16
-  | '10001':REG R17
-  | '10010':REG R18
-  | '10011':REG R19
-  | '10100':REG R20
-  | '10101':REG R21
-  | '10110':REG R22
-  | '10111':REG R23
-  | '11000':REG R24
-  | '11001':REG R25
-  | '11010':REG R26
-  | '11011':REG R27
-  | '11100':REG R28
-  | '11101':REG R29
-  | '11110':REG R30
-  | '11111':REG R31
+    '00000': REG R0
+  | '00001': REG R1
+  | '00010': REG R2
+  | '00011': REG R3
+  | '00100': REG R4
+  | '00101': REG R5
+  | '00110': REG R6
+  | '00111': REG R7
+  | '01000': REG R8
+  | '01001': REG R9
+  | '01010': REG R10
+  | '01011': REG R11
+  | '01100': REG R12
+  | '01101': REG R13
+  | '01110': REG R14
+  | '01111': REG R15
+  | '10000': REG R16
+  | '10001': REG R17
+  | '10010': REG R18
+  | '10011': REG R19
+  | '10100': REG R20
+  | '10101': REG R21
+  | '10110': REG R22
+  | '10111': REG R23
+  | '11000': REG R24
+  | '11001': REG R25
+  | '11010': REG R26
+  | '11011': REG R27
+  | '11100': REG R28
+  | '11101': REG R29
+  | '11110': REG R30
+  | '11111': REG R31
+
+val X = REGHL {regh=REG R27,regl=REG R26}
+val Y = REGHL {regh=REG R29,regl=REG R28}
+val Z = REGHL {regh=REG R31,regl=REG R30}
 
 val d ['bit:1'] = do
  rd <- (query $rd) ^ bit;
- update@{rd=rd};
+ update@{rd=rd}
 end
 
 val r ['bit:1'] = do
  rr <- (query $rr) ^ bit;
- update@{rr=rr};
+ update@{rr=rr}
 end
 
-val K ['bit:1'] = do
- k <- (query $rr) ^ bit;
- update@{rr=rr};
+val k ['bit:1'] = do
+ ck <- (query $ck) ^ bit;
+ update@{ck=ck}
 end
 
 val rd5 = do
@@ -115,27 +115,27 @@ val rr5 = do
  return (register-from-bits rr)
 end
 
-val k = do
- kk <- query $k;
- return (IMM6 kk)
+val ck = do
+ ck <- query $ck;
+ return (IMM6 ck)
 end
 
 val rd5h-rd5l = do
  rd <- query $rd;
  rd-regl <- register-from-bits ('11' ^ rd ^ '0');
  rd-regh <- register-from-bits ('11' ^ rd ^ '1');
- return REGHL {regh=rd-regh,regl=rd-regl};
+ return (REGHL {regh=rd-regh,regl=rd-regl});
 end
 
-val binop cons left right = do
- _left <- left;
- _right <- right;
- return (cons {left=_left, right=_right});
+val binop cons first second = do
+ first <- first;
+ second <- second;
+ return (cons {first=first, second=second});
 end
 
 ### ADC
 ###  - Add with Carry
-val / ['000111 r d d d d d r r r r'] = binop ADC rd5 rr5
+val / ['000111' r d d d d d r r r r] = binop ADC rd5 rr5
 
 ### ADD
 ###  - Add without Carry
@@ -143,4 +143,4 @@ val / ['000011' r d d d d d r r r r] = binop ADD rd5 rr5
 
 ### ADIW
 ###  - Add Immediate to Word
-val / ['10010110' K K d d K K K K] = binop ADIW rd5h-rd5l k
+val / ['10010110' k k d d k k k k] = binop ADIW rd5h-rd5l ck
