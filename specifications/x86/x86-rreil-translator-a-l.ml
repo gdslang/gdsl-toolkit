35ad9e1638056688fd3eb6bd577fb977bbe31aa1
Julian Kranz <julian@juliankranz.de>
2012-11-29 13:08:57 +0100

    X86 RREIL Translator
    - Bug fixes
    - Cleanup

--- specifications/x86/x86-rreil-translator-a-l.ml
+++ specifications/x86/x86-rreil-translator-a-l.ml
@@ -168,8 +168,9 @@ end
 
 val sem-bt-complement base-sz base base-opnd shifted offset-ext = do
   output <- mktemp;
-  andb base-sz output (var shifted) (imm 1);
-  shl base-sz output (var output) (var offset-ext);
+  #andb base-sz output (var shifted) (imm 1);
+  #shl base-sz output (var output) (var offset-ext);
+  shl base-sz output (imm 1) (var offset-ext);
   xorb base-sz output (var output) base;
   dst <- lval base-sz base-opnd;
   write base-sz dst (var output)
@@ -177,8 +178,10 @@ end
 
 val sem-bt-reset base-sz base base-opnd shifted offset-ext = do
   output <- mktemp;
-  xorb base-sz output (imm (0-1)) (imm 1);
-  shl base-sz output (var output) (var offset-ext);
+  shl base-sz output (imm 1) (var offset-ext);
+  xorb base-sz output (var output) (imm (0-1));
+  #xorb base-sz output (imm (0-1)) (imm 1);
+  #shl base-sz output (var output) (var offset-ext);
   andb base-sz output (var output) base;
   dst <- lval base-sz base-opnd;
   write base-sz dst (var output)
