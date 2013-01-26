74330dc0c8c302d697c1248256715019f99850ac
Julian Kranz <julian@juliankranz.de>
2013-01-26 20:03:03 +0100

    X86 RREIL Translator
    - Started to add semantics of: (V)PSHUFB

--- specifications/x86/x86-rreil-translator-m-z.ml
+++ specifications/x86/x86-rreil-translator-m-z.ml
@@ -1467,19 +1467,20 @@ val sem-psadbw x = sem-psadbw-vpsadbw-opnd '0' x.opnd1 x.opnd1 x.opnd2
 val sem-vpsadbw x = sem-psadbw-vpsadbw-opnd '1' x.opnd1 x.opnd2 x.opnd3
 
 val sem-pshufb-vpshufb-opnd avx-encoded opnd1 opnd2 opnd3 = do
-  size <- sizeof1 opnd1;
-  src <- read size opnd2;
-  shuffle-control-mask <- read size opnd3;
-  dst <- lval size opnd1;
+  #size <- sizeof1 opnd1;
+  #src <- read size opnd2;
+  #shuffle-control-mask <- read size opnd3;
+  #dst <- lval size opnd1;
 
-  temp-scm <- mktemp;
+  #temp-scm <- mktemp;
   #=> For each index: mov (logb (divb size 8)) temp-scm shuffle-control-mask;
   #movzx temp-scm to some size?!
 
   #temp-src2 <- mktemp;
   #mov size temp-src2 src2;
 
-  temp-dst <- mktemp;
+  #temp-dst <- mktemp
+  return void
 end
 
 val ps-push opnd-sz opnd = do
