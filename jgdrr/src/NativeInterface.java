ddea046762ae205f6531b8930c0ebb5f3f2ca7ac
Julian Kranz <julian@juliankranz.de>
2013-03-04 19:57:49 +0100

    Jgdrr
    - Continued to implement jgdrr

--- jgdrr/src/NativeInterface.java
+++ jgdrr/src/NativeInterface.java
@@ -8,9 +8,38 @@ public class NativeInterface {
 		return (RReilStatement[])decodeAndTranslateNative(bytes);
 	}
 	
-	private Object assign(Object p) {
-		System.out.println("assign - but in Java :-)");
-		
+	private Object sem_assign(Object lhs, Object rhs) {
+		System.out.println("(j) sem_assign");
+		return null;
+	}
+	
+	private Object sem_load(Object lhs, Object size, Object address) {
+		System.out.println("(j) sem_load");
+		return null;
+	}
+	
+	private Object sem_store(Object lhs, Object rhs) {
+		System.out.println("(j) sem_store");
+		return null;
+	}
+	
+	private Object sem_ite(Object cond, Object then_branch, Object else_branch) {
+		System.out.println("(j) sem_ite");
+		return null;
+	}
+	
+	private Object sem_while(Object cond, Object body) {
+		System.out.println("(j) sem_while");
+		return null;
+	}
+	
+	private Object sem_cbranch(Object cond, Object target_true, Object target_false) {
+		System.out.println("(j) sem_cbranch");
+		return null;
+	}
+	
+	private Object sem_branch(Object branch_hint, Object target) {
+		System.out.println("(j) sem_branch");
 		return null;
 	}
 	
