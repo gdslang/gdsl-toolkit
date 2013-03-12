6cc2297afe2ee57d814ebded6a54090a5071ba8b
Julian Kranz <julian@juliankranz.de>
2013-03-12 17:47:05 +0100

    Jgdrr
    - Continued to implement jgdrr

---
+++ jgdrr/src/rnati/ReilDecodeException.java
@@ -0,0 +1,28 @@
+package rnati;
+
+public class ReilDecodeException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ReilDecodeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReilDecodeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReilDecodeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReilDecodeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
