c4a6e303eda8497c0c70a304007aa8381ce393bb
Julian Kranz <julian@juliankranz.de>
2013-05-13 17:51:13 +0200

    X86 Tester
    - Tester (x86)

---
+++ x86-tester/x86-tester/src/tester.c
@@ -0,0 +1,16 @@
+/*
+ * tester.c
+ *
+ *  Created on: 13.05.2013
+ *      Author: jucs
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <simulator_tracking.h>
+
+uint8_t *tester_code_get(uint8_t *instruction, size_t instruction_length, struct simulator_trace *trace) {
+
+}
