c104aa43bcedc537a94fac3688e9ef6370512e7f
Julian Kranz <julian@juliankranz.de>
2013-08-03 16:22:55 +0200

    ---

--- specifications/x86/c/cli.c
+++ specifications/x86/c/cli.c
@@ -1,45 +1,52 @@
-
 /* vim:cindent:ts=2:sw=2:expandtab */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <dis.h>
+#include <stdint.h>
+#include <gdsl-x86.h>
 
-int main (int argc, char** argv) {
-  __char blob[15];
-  char fmt[1024];
-  __word sz = 15;
-  __obj insn;
-  int i,c;
-  for (i=0;i<sz;i++) {
-     int x = fscanf(stdin,"%x",&c);
-     switch (x) {
-        case EOF:
-           goto done;
-        case 0:
-           __fatal("invalid input; should be in hex form: '0f 0b ..'");
-     }
-     blob[i] = c & 0xff;
-  }
-done:
-  __decode(__decode__,blob,i,&insn);
-  if (___isNil(insn))
-    __fatal("decode failed");
-  else {
-    __pretty(__pretty__,insn,fmt,1024);
-    puts(fmt);
+int main(int argc, char** argv) {
+	void fatal(char *msg) {
+		fprintf(stderr, "%s", msg);
+		exit(1);
+	}
 
-    printf("---------------------------\n");
-   
-    __obj r = __translate(__translate__,insn);
-    if(___isNil(r))
-      __fatal("translate failed");
-    else {
-      __pretty(__rreil_pretty__,r,fmt,1024);
-      puts(fmt);
-    }
-  }
-  
-  return (1);
+	char blob[15];
+	int_t sz = 15;
+	int i, c;
+	for(i = 0; i < sz; i++) {
+		int x = fscanf(stdin, "%x", &c);
+		switch(x) {
+			case EOF:
+				goto done;
+			case 0:
+				fatal("invalid input; should be in hex form: '0f 0b ..'");
+		}
+		blob[i] = c & 0xff;
+	}
+	done: ;
+	state_t state = gdsl_init();
+	gdsl_set_code(state, blob, i, 0);
+
+	if(setjmp(*gdsl_err_tgt(state)))
+		fatal("decode failed");
+	obj_t insn = x86_decode(state);
+
+	string_t fmt = x86_pretty(state, insn);
+	puts(fmt);
+
+	printf("---------------------------\n");
+
+	if(setjmp(*gdsl_err_tgt(state)))
+		fatal("translate failed");
+
+	obj_t rreil = x86_translate(state, insn);
+
+	fmt = x86_rreil_pretty(state, rreil);
+	puts(fmt);
+
+	gdsl_destroy(state);
+
+	return 1;
 }
 
--- specifications/x86/c/sweep.c
+++ specifications/x86/c/sweep.c
@@ -1,85 +1,83 @@
-
 /* vim:cindent:ts=2:sw=2:expandtab */
 
-#include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
-#include <dis.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <gdsl-x86.h>
 #include <sys/resource.h>
 
-int main (int argc, char** argv) {
-    const rlim_t kStackSize = 64L * 1024L * 1024L;   // min stack size = 64 Mb
-    struct rlimit rl;
-    int result;
+int main(int argc, char** argv) {
+	void fatal(char *msg) {
+		fprintf(stderr, "%s", msg);
+		exit(1);
+	}
 
-    result = getrlimit(RLIMIT_STACK, &rl);
-    if (result == 0)
-    {
-        if (rl.rlim_cur < kStackSize)
-        {
-            rl.rlim_cur = kStackSize;
-            result = setrlimit(RLIMIT_STACK, &rl);
-            if (result != 0)
-            {
-                fprintf(stderr, "setrlimit returned result = %d\n", result);
-            }
-        }
-    }
+	const rlim_t kStackSize = 64L * 1024L * 1024L; // min stack size = 64 Mb
+	struct rlimit rl;
+	int result;
 
-  size_t size = 256*1024;
-  char fmt[size];
-  __obj insn;
+	result = getrlimit(RLIMIT_STACK, &rl);
+	if(result == 0) {
+		if(rl.rlim_cur < kStackSize) {
+			rl.rlim_cur = kStackSize;
+			result = setrlimit(RLIMIT_STACK, &rl);
+			if(result != 0) {
+				fprintf(stderr, "setrlimit returned result = %d\n", result);
+			}
+		}
+	}
 
-  if(argc != 4) {
-    printf("Usage: sweep file offset length\n");
-    return 1;
-  }
+	if(argc != 4) {
+		printf("Usage: sweep file offset length\n");
+		return 1;
+	}
 
-  size_t offset;
-  sscanf(argv[2], "%zu", &offset);
-  size_t length;
-  sscanf(argv[3], "%zu", &length);
+	size_t offset;
+	sscanf(argv[2], "%zu", &offset);
+	size_t length;
+	sscanf(argv[3], "%zu", &length);
 
-  FILE *f = fopen(argv[1], "r");
-  if(!f) {
-    printf("Unable to open file.\n");
-    return 1;
-  }
-  fseek(f, offset, SEEK_SET);
+	FILE *f = fopen(argv[1], "r");
+	if(!f) {
+		printf("Unable to open file.\n");
+		return 1;
+	}
+	fseek(f, offset, SEEK_SET);
 
-  size_t buffer_size = length + 15;
-  unsigned char *buffer = (unsigned char*)malloc(buffer_size);
-  size_t buffer_length = fread(buffer, 1, buffer_size, f);
+	size_t buffer_size = length + 15;
+	char *buffer = (char*)malloc(buffer_size);
+	size_t buffer_length = fread(buffer, 1, buffer_size, f);
 
-  __obj state = __createState(buffer, buffer_length, 0, 0);
+	state_t state = gdsl_init();
+	gdsl_set_code(state, buffer, buffer_length, 0);
 
-  //uint64_t consumed = 0;
-  while(__getBlobIndex(state) + 15 < buffer_length) {
-    printf("++++++++++++ DECODING NEXT INSTRUCTION ++++++++++++\n");
-    insn = __runMonadicNoArg(__decode__, &state);
-    
-    //consumed += __decode(__decode__,buffer+consumed,buffer_length - consumed,&insn);
-    //printf("Consumed: %lu\n", consumed);
-    if (___isNil(insn))
-      __fatal("Decode failed");
-    else {
-      __pretty(__pretty__,insn,fmt,size);
-      puts(fmt);
-  
-      printf("---------------------------\n");
-     
-      __obj r = __runMonadicOneArg(__translate__, &state, insn);
-      //__obj r = __translate(__translate__,insn);
-      if(___isNil(r))
-        __fatal("Translate failed");
-      else {
-        __pretty(__rreil_pretty__,r,fmt,size);
-        puts(fmt);
-      }
-    }
-  }
+	//uint64_t consumed = 0;
+	while(gdsl_get_ip_offset(state) + 15 < buffer_length) {
+		printf("++++++++++++ DECODING NEXT INSTRUCTION ++++++++++++\n");
 
-  
-  return (1);
+		if(setjmp(*gdsl_err_tgt(state)))
+			fatal("Decode failed");
+		obj_t insn = x86_decode(state);
+
+		string_t fmt = x86_pretty(state, insn);
+		puts(fmt);
+
+		printf("---------------------------\n");
+
+		if(setjmp(*gdsl_err_tgt(state)))
+			fatal("Translate failed");
+		obj_t rreil = x86_translate(state, insn);
+//		__obj r = __runMonadicOneArg(__translate__, &state, insn);
+		//__obj r = __translate(__translate__,insn);
+
+		fmt = x86_rreil_pretty(state, rreil);
+		puts(fmt);
+
+		gdsl_reset_heap(state);
+	}
+
+	gdsl_destroy(state);
+
+	return 1;
 }
 
