3f3b39e1a597a2eafb162a1ed9fb3bd84dac328f
Julian Kranz <julian@juliankranz.de>
2013-08-07 13:29:46 +0200

    ---

--- tools/tardet/examples/example_a.c
+++ tools/tardet/examples/example_a.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	goto end;
-	start:
-	__asm__ ("mov $999, %rax\n"
-"add $42, %rax\n"
-"mov $62, %ah\n"
-"jmp %rax\n");
-	end:;
+void _exit() {
+	exit(0);
+}
 
+int main(void) {
 	FILE *f = fopen("example_a.bin", "w");
 	if(!f)
 		return 1;
@@ -18,6 +14,15 @@ int main(void) {
 		fwrite(i, 1, 1, f);
 
 	fclose(f);
+
+	_exit();
+
+	start:
+	asm ("mov $999, %rax\n"
+"add $42, %rax\n"
+"mov $62, %ah\n"
+"jmp %rax\n");
+	end:;
 
 	return 0;
 }
