78b4966e1bddcfcb30de8140f8b6564a6bac2cc4
Julian Kranz <julian.kranz@in.tum.de>
2013-10-18 15:41:10 +0200

    ---

--- specifications/rreil/rreil-opt.ml
+++ specifications/rreil/rreil-opt.ml
@@ -1,25 +1,25 @@
-export = translateBlockOptimized
+export = decode-translate-blockOptimized
 
 type sem_preservation =
    SEM_PRESERVATION_EVERYWHERE
  | SEM_PRESERVATION_BLOCK
  | SEM_PRESERVATION_CONTEXT
 
-val translateBlockOptimized config limit pres = case pres of
+val decode-translate-blockOptimized config limit pres = case pres of
    SEM_PRESERVATION_EVERYWHERE: do
-     translated <- translateBlock config limit;
+     translated <- decode-translate-block config limit;
      clean <- cleanup translated;
      return clean
    end
  | SEM_PRESERVATION_BLOCK: do
-     translated <- translateBlock config limit;
+     translated <- decode-translate-block config limit;
      lv-result <- liveness translated;
      live <- query $live;
      clean <- cleanup live;
      return clean
    end
  | SEM_PRESERVATION_CONTEXT: do
-     translated <- translateSuperBlock config limit;
+     translated <- decode-translate-super-block config limit;
      lv-result <- liveness_super translated;
      live <- query $live;
      clean <- cleanup live;
