dced8371f2ee5749be67456a939c757b558a93be
Julian Kranz <julian.kranz@in.tum.de>
2014-01-15 22:25:34 +0100

    Comparison of rreil_id structures

--- libs/rreil/src/rreil/rreil_id.c
+++ libs/rreil/src/rreil/rreil_id.c
@@ -31,3 +31,34 @@ char rreil_id_equals(struct rreil_id *a, struct rreil_id *b) {
 	}
 	return 0;
 }
+
+static char compare_long_unsinged(long unsigned int a, long unsigned int b) {
+	if(a == b)
+		return 0;
+	else if(a < b)
+		return -1;
+	return 1;
+}
+
+char rreil_id_compare(struct rreil_id *a, struct rreil_id *b) {
+	if(a->type != b->type)
+		return compare_long_unsinged(a->type, b->type);
+	switch(a->type) {
+		case RREIL_ID_TYPE_SHARED: {
+			return compare_long_unsinged(a->shared, b->shared);
+		}
+		case RREIL_ID_TYPE_TEMPORARY: {
+			return compare_long_unsinged(a->temporary, b->temporary);
+		}
+#ifdef GDSL_X86
+		case RREIL_ID_TYPE_X86: {
+			return compare_long_unsinged(a->x86, b->x86);
+		}
+#else
+		case RREIL_ID_TYPE_ARCH: {
+			return compare_long_unsinged(a->arch, b->arch);
+		}
+#endif
+	}
+	return 0;
+}
