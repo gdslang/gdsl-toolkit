ad3ba4813cd764af2f71a598f287fa0b1129dc59
Julian Kranz <kranzj@in.tum.de>
2014-02-26 13:46:37 +0100

    libjgdsl: Documentation

--- libs/jgdsl/src/gdsl/translator/Translator.java
+++ libs/jgdsl/src/gdsl/translator/Translator.java
@@ -8,19 +8,44 @@ import gdsl.rreil.IRReilBuilder;
 import gdsl.rreil.IRReilCollection;
 import gdsl.rreil.statement.IStatement;
 
+/**
+ * This class represents the translator interface of Gdsl.
+ * 
+ * @author Julian Kranz
+ */
 public class Translator {
   private Gdsl gdsl;
   private BuilderBackend backend;
 
+  /**
+   * Construct the Translator
+   * 
+   * @param gdsl the corresponding {@link Gdsl} object
+   * @param builder the {@link IRReilBuilder} object that builds the RReil AST using callback methods
+   */
   public Translator (Gdsl gdsl, IRReilBuilder builder) {
     this.gdsl = gdsl;
     this.backend = new BuilderBackend(builder);
   }
 
+  /**
+   * Translate one instruction object
+   * 
+   * @param insn the instruction to translate
+   * @return a collection of RReil statements built by the associated RReil builder
+   */
   public IRReilCollection<IStatement> translate (Instruction insn) {
     return backend.translate(gdsl.getFrontendPtr(), gdsl.getGdslStatePtr(), insn.getInsnPtr());
   }
 
+  /**
+   * Decode and translate one basic block
+   * 
+   * @param limit the maximal number of bytes to read from the input stream
+   * @param preservation the required semantics preservation
+   * 
+   * @return a collection of RReil statements built by the associated RReil builder
+   */
   public IRReilCollection<IStatement> translateOptimizeBlock (long limit, SemPres preservation) {
     Frontend frontend = gdsl.getFrontend();
     if (frontend.isConfigured())
