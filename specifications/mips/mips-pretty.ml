7710c15ddddf0040ed65bfcdc32edb3cca9d8c73
Benedikt Geßele <benedikt@gessele.de>
2014-06-05 15:22:41 +0200

    MIPS pretty update

--- specifications/mips/mips-pretty.ml
+++ specifications/mips/mips-pretty.ml
@@ -4,41 +4,74 @@ export = pretty
 
 val pretty i = show/instruction i
 
-val show/unop x = show/operand x.operand
-val show/binop x = show/operand x.first +++ ", " +++ show/operand x.second
-val show/ternop x = show/operand x.first +++ ", " +++ show/operand x.second +++ ", " +++ show/operand x.third
-val show/quadop x = show/operand x.first +++ ", " +++ show/operand x.second +++ ", " +++ show/operand x.third +++ ", " +++ show/operand x.fourth
-val show/quinop x = show/operand x.first +++ ", " +++ show/operand x.second +++ ", " +++ show/operand x.third +++ ", " +++ show/operand x.fourth +++ ", " +++ show/operand x.fifth
-
 val -++ a b = a +++ " " +++ b
 
-val show/operand opnd = 
+val show/leftop opnd = 
    case opnd of
       GPR r: show/register r
     | FPR r: show/register r
-    | IMM imm: show/operand/imm imm
    end
 
-val show/operand/imm imm =
+val show/rightop opnd = 
+   case opnd of
+      LEFTOP l: show/leftop l
+    | IMM imm: show/immediate imm
+   end
+
+val show/immediate imm =
    case imm of
       IMM16 x: show-int (zx x)
+    | SIZE i: show-int i
   end
-
-val show/instruction i =
-   case i of
-      ADD x: "ADD" -++ show/ternop x    
-   end
 
 val show/register r =
    case r of
-      R x: "r" +++ show-int x
-    | HI: "HI"
-    | LO: "LO"
-    | PC: "PC"
+      ZERO: "zero"
+    | AT: "at"
+    | V0: "v0"
+    | V1: "v1"
+    | A0: "a0"
+    | A1: "a1"
+    | A2: "a2"
+    | A3: "a3"
+    | A4: "a4"
+    | A5: "a5"
+    | A6: "a6"
+    | A7: "a7"
+    | T4: "t4"
+    | T5: "t5"
+    | T6: "t6"
+    | T7: "t7"
+    | S0: "s0"
+    | S1: "s1"
+    | S2: "s2"
+    | S3: "s3"
+    | S4: "s4"
+    | S5: "s5"
+    | S6: "s6"
+    | S7: "s7"
+    | T8: "t8"
+    | T9: "t9"
+    | K0: "k0"
+    | K1: "k1"
+    | GP: "gp"
+    | SP: "sp"
+    | S8: "s8"
+    | RA: "ra"
+    | HI: "hi"
+    | LO: "lo"
+    | PC: "pc"
     | F x: "f" +++ show-int x
-    | FIR: "FIR"
-    | FCCR: "FCCR"
-    | FEXR: "FEXR"
-    | FENR: "FENR"
-    | FCSR: "FCSR"
+    | FIR: "fir"
+    | FCCR: "fccr"
+    | FEXR: "fexr"
+    | FENR: "fenr"
+    | FCSR: "fcsr"
    end
+
+val show/ternop x = show/leftop x.destination +++ ", " +++ show/rightop x.source1 +++ ", " +++ show/rightop x.source2
+
+val show/instruction i =
+   case i of
+      ADD x: "ADD" -++ show/ternop x
+  end
--- specifications/mips/mips.ml
+++ specifications/mips/mips.ml
@@ -1,40 +1,111 @@
 granularity = 32
 export = config-default decode
 
-
-### ADD
-###  - ADD
-val / ['000000 gr:5 gt:5 gg:5 00000 100000'] = ternop ADD (return (GPR (gpr-from-bits gr))) (return (GPR (gpr-from-bits gt))) (return (GPR (gpr-from-bits gg)))
-
-
-val config-default = ''
-
-val decode config = do
-  update@{ck=''};
-  /
-end
-
 val force-int-for-decode-config = decode config-default
 
 type imm =
    IMM16 of 16
+ | SIZE of int
 
-type operand =
+type leftop =
    GPR of register
  | FPR of register
+
+type rightop =
+   LEFTOP of leftop
  | IMM of imm
 
-type quinop = {first:operand,second:operand,third:operand,fourth:operand,fifth:operand}
-type quadop = {first:operand,second:operand,third:operand,fourth:operand}
-type ternop = {first:operand,second:operand,third:operand}
-type binop = {first:operand,second:operand}
-type unop = {operand:operand}
+val right leftop = do
+  leftop <- leftop;
+  return (LEFTOP leftop)
+end
+
+val config-default = ''
+
+val decode config = do
+  update@{ck='',rs='',rt='',rd='',immediate=''};
+  /
+end
+
+### ADD
+###  - ADD
+val / ['000000 /rs /rt /rd 00000 100000'] = ternop ADD rd (right rs) (right rt)
+
+val rs = do
+  rs <- query $rs;
+  update @{rs=''};
+  return (GPR (gpr-from-bits rs))
+end
+
+val rt = do
+  rt <- query $rt;
+  update @{rt=''};
+  return (GPR (gpr-from-bits rt))
+end
+
+
+val rd = do
+  rd <- query $rd;
+  update @{rd=''};
+  return (GPR (gpr-from-bits rd))
+end
+
+val immediate = do
+  immediate <- query $immediate;
+  update @{immediate=''};
+  return (IMM (IMM16 immediate))
+end
+
+val /rs ['rs:5'] = update@{rs=rs}
+val /rt ['rt:5'] = update@{rt=rt}
+val /rd ['rd:5'] = update@{rd=rd}
+val /immediate ['immediate:16'] = update@{immediate=immediate}
+
+type ternop = {destination:leftop,source1:rightop,source2:rightop}
+
+val ternop cons destination source1 source2 = do
+ destination <- destination;
+ source1 <- source1;
+ source2 <- source2;
+ return (cons {destination=destination, source1=source1, source2=source2})
+end
 
 type instruction =
    ADD of ternop
 
 type register =
-   R of int
+   ZERO
+ | AT
+ | V0
+ | V1
+ | A0
+ | A1
+ | A2
+ | A3
+ | A4
+ | A5
+ | A6
+ | A7
+ | T4
+ | T5
+ | T6
+ | T7
+ | S0
+ | S1
+ | S2
+ | S3
+ | S4
+ | S5
+ | S6
+ | S7
+ | T8
+ | T9
+ | K0
+ | K1
+ | GP
+ | SP
+ | S8
+ | RA
  | HI
  | LO
  | PC
@@ -47,45 +118,40 @@ type register =
  | FENR
  | FCSR
 
-val gpr-from-bits bits = (R (zx bits))
+val gpr-from-bits bits =
+ case bits of
+    '00000': ZERO
+  | '00001': AT
+  | '00010': V0
+  | '00011': V1
+  | '00100': A0
+  | '00101': A1
+  | '00110': A2
+  | '00111': A3
+  | '01000': A4
+  | '01001': A5
+  | '01010': A6
+  | '01011': A7
+  | '01100': T4
+  | '01101': T5
+  | '01110': T6
+  | '01111': T7
+  | '10000': S0
+  | '10001': S1
+  | '10010': S2
+  | '10011': S3
+  | '10100': S4
+  | '10101': S5
+  | '10110': S6
+  | '10111': S7
+  | '11000': T8
+  | '11001': T9
+  | '11010': K0
+  | '11011': K1
+  | '11100': GP
+  | '11101': SP
+  | '11110': S8
+  | '11111': RA
+ end
+
 val fpr-from-bits bits = (F (zx bits))
-
-val quinop cons first second third fourth fifth = do
- first <- first;
- second <- second;
- third <- third;
- fourth <- fourth;
- fifth <- fifth;
- return (cons {first=first, second=second, third=third, fourth=fourth, fifth=fifth})
-end
-
-val quadop cons first second third fourth = do
- first <- first;
- second <- second;
- third <- third;
- fourth <- fourth;
- return (cons {first=first, second=second, third=third, fourth=fourth})
-end
-
-val ternop cons first second third = do
- first <- first;
- second <- second;
- third <- third;
- return (cons {first=first, second=second, third=third})
-end
-
-val binop cons first second = do
- first <- first;
- second <- second;
- return (cons {first=first, second=second})
-end
-
-val unop cons operand = do
- operand <- operand;
- return (cons {operand=operand})
-end
-
-val nullop cons = do
- return cons
-end
-
