a4148096dd172386cb209a0018c39b25f24fbaed
Julian Kranz <kranzj@in.tum.de>
2014-07-01 18:30:53 +0200

    Generic ASM

--- specifications/asm/asm-pretty.ml
+++ specifications/asm/asm-pretty.ml
@@ -1,50 +1,52 @@
-val asm-pretty ai = ai.mnemonic +++ show/flags ai.flags -++ show/opnds ai.opnds
+#val asm-pretty ai = ai.mnemonic +++ show/flags ai.flags -++ show/opnds ai.opnds
+#
+#val show/flags flags = case flags of
+#   CONDITION c: ".<=" +++ show/opnd c +++ "=>"
+# | SFLAGS s: from-string-lit s
+# | NOFLAGS: ""
+#end
+#
+#val show/opnds opnds = case opnds of
+#   OPNDS_NIL: ""
+# | OPNDS_CONS c: show/opnd c.hd +++ (case c.tl of OPNDS_NIL: "" | _: " " +++ show/opnds c.tl end)
+#end
+#
+#val show/opnd opnd = case opnd of
+#   LSO l: show/lso l
+# | RSO r: show/rso r
+# | CATEGORY c: from-string-lit c.category +++ ":" +++ show/opnd c.opnd
+#end
+#
+#val show/immediate i = case i.signedness of
+#   UNSIGNED: show-int (imm-as-uint i.immediate)
+# | SIGNED: show-int (imm-as-int i.immediate)
+# | UNSPEC: show-int (imm-as-uint i.immediate) #Todo: show as hex string
+#end
+#
+#val show/rso r = case r of
+#   FLO l: show/lso l
+# | IMM i: show/immediate i
+# | SUM s: "(" +++ show/rso s.lhs -++ "+" -++ show/rso s.rhs +++ ")"
+# | SCALE s: show-int s.factor +++ "*" +++ show/rso s.rhs
+# | REL a: "(?+ " -++ show/rso a +++ ")"
+#end
+#
+#val show/lso l = case l of
+#   REGISTER r: show/register r
+# | MEMORY m: show/memory m
+# | POST_OP po: "(" +++ show/lso po.lso +++ " [" +++ show/lso po.lso +++ " := " +++ show/rso po.expr +++ "])"
+# | PRE_OP pr: "([" +++ show/lso pr.lso +++ " := " +++ show/rso pr.expr +++ "] " +++ show/lso pr.lso +++ ")"
+#end
+#
+#val show/register r = case r of
+#   REGISTER_NAME n: from-string-lit n
+# | REGISTER_SO r: from-string-lit r.mnemonic +++ (if (r.offset === 0) == '0' then
+#     "." +++ show-int r.offset
+#   else
+#     ""
+#   ) +++ "/" +++ show-int r.size
+#end
+#
+#val show/memory m = "*" +++ show/opnd m.pointer
 
-val show/flags flags = case flags of
-   CONDITION c: ".<=" +++ show/opnd c +++ "=>"
- | SFLAGS s: from-string-lit s
- | NOFLAGS: ""
-end
-
-val show/opnds opnds = case opnds of
-   OPNDS_NIL: ""
- | OPNDS_CONS c: show/opnd c.hd +++ (case c.tl of OPNDS_NIL: "" | _: " " +++ show/opnds c.tl end)
-end
-
-val show/opnd opnd = case opnd of
-   LSO l: show/lso l
- | RSO r: show/rso r
- | CATEGORY c: from-string-lit c.category +++ ":" +++ show/opnd c.opnd
-end
-
-val show/immediate i = case i.signedness of
-   UNSIGNED: show-int (imm-as-uint i.immediate)
- | SIGNED: show-int (imm-as-int i.immediate)
- | UNSPEC: show-int (imm-as-uint i.immediate) #Todo: show as hex string
-end
-
-val show/rso r = case r of
-   FLO l: show/lso l
- | IMM i: show/immediate i
- | SUM s: "(" +++ show/rso s.lhs -++ "+" -++ show/rso s.rhs +++ ")"
- | SCALE s: show-int s.factor +++ "*" +++ show/rso s.rhs
- | REL a: "(?+ " -++ show/rso a +++ ")"
-end
-
-val show/lso l = case l of
-   REGISTER r: show/register r
- | MEMORY m: show/memory m
- | POST_OP po: "(" +++ show/lso po.lso +++ " [" +++ show/lso po.lso +++ " := " +++ show/rso po.expr +++ "])"
- | PRE_OP pr: "([" +++ show/lso pr.lso +++ " := " +++ show/rso pr.expr +++ "] " +++ show/lso pr.lso +++ ")"
-end
-
-val show/register r = case r of
-   REGISTER_NAME n: from-string-lit n
- | REGISTER_SO r: from-string-lit r.mnemonic +++ (if (r.offset === 0) == '0' then
-     "." +++ show-int r.offset
-   else
-     ""
-   ) +++ "/" +++ show-int r.size
-end
-
-val show/memory m = "*" +++ show/opnd m.pointer
+val blllaaahhh = 99
--- specifications/asm/asm.ml
+++ specifications/asm/asm.ml
@@ -1,122 +1,67 @@
-type insn = {length:int, mnemonic:string, flags:flags, opnds:opnd_list}
+type asm-insn = {length:int, mnemonic:string, flags:asm-annotation-list, opnds:asm-opnd-list}
 
-type opnd_list =
-   OPNDS_NIL
- | OPNDS_CONS of {hd:opnd, tl:opnd_list}
+type asm-opnd-list =
+   ASM_OPNDS_NIL
+ | ASM_OPNDS_CONS of {hd:asm-opnd, tl:asm-opnd-list}
 
-type rso =
-   FLO of lso
- | IMM of {signedness:signedness, immediate:immediate}
- | SUM of {lhs:rso, rhs:rso}
- | SCALE of {factor:int, rhs:rso}
- | REL of rso
+type asm-opnd =
+   ASM_REGISTER of asm-register
+ | ASM_MEMORY of asm-memory
+ | ASM_IMM of asm-immediate
+ | ASM_POST_OP of {expr:asm-opnd, opnd:asm-opnd}
+ | ASM_PRE_OP of {expr:asm-opnd, opnd:asm-opnd}
+ | ASM_REL of asm-opnd
+ | ASM_ANNOTATION of asm-annotation
+ | ASM_SUM of {lhs:asm-opnd, rhs:asm-opnd}
+ | ASM_SCALE of {factor:int, rhs:asm-opnd}
 
-#Todo: Gemeiner Typ für Ops
-#Todo: Prefixe für Namen
+type asm-register =
+   ASM_REGISTER_NAME of string
+ | ASM_REGISTER_SO of {mnemonic:string, size:int, offset:int}
 
-type lso =
-   REGISTER of register
- | MEMORY of {deref-size:int, pointer:opnd}
- | POST_OP of {expr:rso, lso:lso}
- | PRE_OP of {expr:rso, lso:lso}
+type asm-annotation-list =
+   ASM_ANNOTATIONS_NIL
+ | ASM_ANNOTATIONS_CONS of {hd:asm-annotation, tl:asm-annotation-list}
 
-type opnd =
-   LSO of lso
- | RSO of rso
- | CATEGORY of {category:string, opnd:opnd}
+type asm-annotation =
+   ASM_ANN_STRING of string
+ | ASM_ANN_FUNCTION of {name:string, args:asm-opnd-list}
+ | ASM_ANN_OPND of {name:string, opnd:asm-opnd}
 
-type register =
-   REGISTER_NAME of string
- | REGISTER_SO of {mnemonic:string, size:int, offset:int}
+type asm-memory = {deref-size:int, pointer:asm-opnd}
 
-type signedness =
-   UNSIGNED
- | SIGNED
- | UNSPEC
+type asm-immediate =
+   ASM_IMMEDIATE of int
+ | ASM_UNKNOWN_SIGNEDNESS of {value:int, size:int}
 
-#Todo: int + size statt v
-
-type immediate =
-   I1 of 1
- | I8 of 8
- | I16 of 16
- | I24 of 24
- | I32 of 32
- | I40 of 40
- | I48 of 48
- | I56 of 56
- | I64 of 64
- | I128 of 128
- | I256 of 256
- | I512 of 512
-
-type flags =
-   CONDITION of opnd
- | SFLAGS of string
- | NOFLAGS
-
-val imm-as-uint imm = case imm of
-   I1 i: zx i
- | I8 i: zx i
- | I16 i: zx i
- | I24 i: zx i
- | I32 i: zx i
- | I40 i: zx i
- | I48 i: zx i
- | I56 i: zx i
- | I64 i: zx i
- | I128 i: zx i
- | I256 i: zx i
- | I512 i: zx i
-end
-
-val imm-as-int imm = case imm of
-   I1 i: sx i
- | I8 i: sx i
- | I16 i: sx i
- | I24 i: sx i
- | I32 i: sx i
- | I40 i: sx i
- | I48 i: sx i
- | I56 i: sx i
- | I64 i: sx i
- | I128 i: sx i
- | I256 i: sx i
- | I512 i: sx i
-end
-
-val asm-insn l m o = {length=l, mnemonic=m, flags=NOFLAGS, opnds=o}
+val asm-insn l m o = {length=l, mnemonic=m, flags=ASM_ANNOTATIONS_NIL, opnds=o}
 val asm-insn-flags l m f o = {length=l, mnemonic=m, flags=f, opnds=o}
 
-val asm-opnds-none = OPNDS_NIL
-val asm-opnds-one hd = OPNDS_CONS {hd=hd, tl=OPNDS_NIL}
-val asm-opnds-more hd tl = OPNDS_CONS {hd=hd, tl=tl}
+val asm-opnds-none = ASM_OPNDS_NIL
+val asm-opnds-one hd = ASM_OPNDS_CONS {hd=hd, tl=ASM_OPNDS_NIL}
+val asm-opnds-more hd tl = ASM_OPNDS_CONS {hd=hd, tl=tl}
 
-val asm-froml l = FLO l
-val asm-rreg r = FLO (asm-lreg r)
-val asm-rmem dsize ptr = FLO (asm-lmem dsize ptr)
-val asm-rpo expr lso = FLO (asm-lpo expr lso)
-val asm-rpr expr lso = FLO (asm-lpr expr lso)
-val asm-rimm simm = IMM simm
-val asm-rsum l r = SUM {lhs=l, rhs=r}
-val asm-rscale f r = SCALE {factor=f, rhs=r}
-val asm-rel o = REL o
+val asm-reg r = ASM_REGISTER r
+val asm-mem dsize ptr = ASM_MEMORY {deref-size=dsize, pointer=ptr}
+val asm-imm simm = ASM_IMM simm
+val asm-po expr opnd = ASM_POST_OP {expr=expr, opnd=opnd}
+val asm-pr expr opnd = ASM_PRE_OP {expr=expr, opnd=opnd}
+val asm-rel o = ASM_REL o
+val asm-annotation a = ASM_ANNOTATION a
+val asm-sum l r = ASM_SUM {lhs=l, rhs=r}
+val asm-scale f r = ASM_SCALE {factor=f, rhs=r}
 
-val asm-lreg r = REGISTER r
-val asm-lmem dsize ptr = MEMORY {deref-size=dsize, pointer=ptr}
-val asm-lpo expr lso = POST_OP {expr=expr, lso=lso}
-val asm-lpr expr lso = PRE_OP {expr=expr, lso=lso}
+val asm-register mnemonic = ASM_REGISTER_NAME mnemonic
+val asm-register-so mnemonic size offset = ASM_REGISTER_SO {mnemonic=mnemonic, size=size, offset=offset}
 
-val asm-lopnd l = LSO l
-val asm-ropnd r = RSO r
-val asm-copnd c o = CATEGORY {category=c, opnd=o}
+val asm-annotations-none = ASM_ANNOTATIONS_NIL
+val asm-annotations-one hd = ASM_ANNOTATIONS_CONS {hd=hd, tl=ASM_ANNOTATIONS_NIL}
+val asm-annotations-more hd tl = ASM_ANNOTATIONS_CONS {hd=hd, tl=tl}
 
-val asm-register mnemonic = REGISTER_NAME mnemonic
-val asm-register-so mnemonic size offset = REGISTER_SO {mnemonic=mnemonic, size=size, offset=offset}
+val asm-ann-string s = ASM_ANN_STRING s
+val asm-ann-function name args = ASM_ANN_FUNCTION {name=name, args=args}
+val asm-ann-function-unary name arg = ASM_ANN_FUNCTION {name=name, args=asm-opnds-one arg}
+val asm-ann-opnd name opnd = ASM_ANN_OPND {name=name, opnd=opnd}
 
-val asm-int imm = {signedness=SIGNED, immediate=imm}
-val asm-uint imm = {signedness=UNSIGNED, immediate=imm}
-val asm-imm imm = {signedness=UNSPEC, immediate=imm}
-
-val asm-condition opnd = CONDITION opnd
-val asm-flags s = SFLAGS s
+val asm-immediate i = ASM_IMMEDIATE i
+val asm-immediate-unk value size = ASM_UNKNOWN_SIGNEDNESS {value=value, size=size}
