e0fe1d273baec5f3170428d069614753b44d2f0d
Julian Kranz <kranzj@in.tum.de>
2014-07-14 12:45:07 +0200

    AVR

--- specifications/avr/avr-pretty.ml
+++ specifications/avr/avr-pretty.ml
@@ -1,6 +1,6 @@
 # vim:ts=3:sw=3:expandtab
 
-export = pretty pretty-operand pretty-mnemonic
+export = pretty
 
 val pretty i = show/instruction i
 
@@ -30,171 +30,28 @@ val show/operand opnd =
     | OPDI op: show/operand op.op +++ "+" +++ show/operand/imm op.imm
    end
 
-val show/operand/imm imm =
-   case imm of
-      IMM3 x: show-int (zx x)
-    | IMM4 x: show-int (zx x)
-    | IMM6 x: show-int (zx x)
-    | IMM7 x: show-int (zx x)
-    | IMM8 x: show-int (zx x)
-    | IMM12 x: show-int (zx x)
-    | IMM16 x: show-int (zx x)
-    | IMM22 x: show-int (zx x)
-    | IMMi i: show-int i
-  end
-
-val show/operands insn_cl =
-  case insn_cl of
-     TERNOP o: show/ternop o
-   | BINOP o: show/binop o
-   | UNOP o: show/unop o
-   | NOOP: ""
-  end
-
-val pretty-operand insn i = let
-  val ith3 o = case i of
-     0: show/operand o.first
-   | 1: show/operand o.second
-   | 2: show/operand o.third
-  end
-
-  val ith2 o = case i of
-     0: show/operand o.first
-   | 1: show/operand o.second
-  end
-
-  val ith1 o = case i of
-     0: show/operand o.operand
-  end
-in
-  case (classify insn) of
-     TERNOP o: ith3 o
-   | BINOP o: ith2 o
-   | UNOP o: ith1 o
-  end
+val show/operand/imm imm = case imm of
+   IMM3 x: show-int (zx x)
+ | IMM4 x: show-int (zx x)
+ | IMM6 x: show-int (zx x)
+ | IMM7 x: show-int (zx x)
+ | IMM8 x: show-int (zx x)
+ | IMM12 x: show-int (zx x)
+ | IMM16 x: show-int (zx x)
+ | IMM22 x: show-int (zx x)
+ | IMMi i: show-int i
 end
 
-val show/instruction i = let
-  val insn_cl = classify i
-in
- show/mnemonic i -++ show/operands insn_cl
-end
-
-val show/mnemonic i =
-   case i of
-      ADC x: "ADC"
-    | ADD x: "ADD"
-    | ADIW x: "ADIW"
-    | AND x: "AND"
-    | ANDI x: "ANDI"
-    | ASR x: "ASR"
-    | BLD x: "BLD"
-    | BRCC x: "BRCC"
-    | BRCS x: "BRCS"
-    | BREAK: "BREAK"
-    | BREQ x: "BREQ"
-    | BRGE x: "BRGE"
-    | BRHC x: "BRHC"
-    | BRHS x: "BRHS"
-    | BRID x: "BRID"
-    | BRIE x: "BRIE"
-    | BRLT x: "BRLT"
-    | BRMI x: "BRMI"
-    | BRNE x: "BRNE"
-    | BRPL x: "BRPL"
-    | BRTC x: "BRTC"
-    | BRTS x: "BRTS"
-    | BRVC x: "BRVC"
-    | BRVS x: "BRVS"
-    | BST x: "BST"
-    | CALL x: "CALL"
-    | CBI x: "CBI"
-    | CBR x: "CBR"
-    | CLC: "CLC" 
-    | CLH: "CLH"
-    | CLI: "CLI"
-    | CLN: "CLN"
-    | CLR x: "CLR"
-    | CLS: "CLS"
-    | CLT: "CLT"
-    | CLV: "CLV"
-    | CLZ: "CLZ"
-    | COM x: "COM"
-    | CP x: "CP"
-    | CPC x: "CPC"
-    | CPI x: "CPI"
-    | CPSE x: "CPSE"
-    | DEC x: "DEC"
-    | DES x: "DES"
-    | EICALL: "EICALL"
-    | EIJMP: "EIJMP"
-    | ELPM x: "ELPM"
-    | EOR x: "EOR"
-    | FMUL x: "FMUL"
-    | FMULS x: "FMULS"
-    | FMULSU x: "FMULSU"
-    | ICALL: "ICALL"
-    | IJMP: "IJMP"
-    | IN x: "IN"
-    | INC x: "INC"
-    | JMP x: "JMP"
-    | LAC x: "LAC"
-    | LAS x: "LAS"
-    | LAT x: "LAT"
-    | LD x: "LD"
-    | LDI x: "LDI"
-    | LDS x: "LDS"
-    | LPM x: "LPM"
-    | LSL x: "LSL"
-    | LSR x: "LSR"
-    | MOV x: "MOV"
-    | MOVW x: "MOVW"
-    | MUL x: "MUL"
-    | MULS x: "MULS"
-    | MULSU x: "MULSU"
-    | NEG x: "NEG"
-    | NOP: "NOP"
-    | OR x: "OR"
-    | ORI x: "ORI"
-    | OUT x: "OUT"
-    | POP x: "POP"
-    | PUSH x: "PUSH"
-    | RCALL x: "RCALL"
-    | RET: "RET"
-    | RETI: "RETI"
-    | RJMP x: "RJMP"
-    | ROL x: "ROL"
-    | ROR x: "ROR"
-    | SBC x: "SBC"
-    | SBCI x: "SBCI"
-    | SBI x: "SBI"
-    | SBIC x: "SBIC"
-    | SBIS x: "SBIS"
-    | SBIW x: "SBIW"
-    | SBR x: "SBR"
-    | SBRC x: "SBRC"
-    | SBRS x: "SBRS"
-    | SEC: "SEC"
-    | SEH: "SEH"
-    | SEI: "SEI"
-    | SEN: "SEN"
-    | SES: "SES"
-    | SET: "SET"
-    | SEV: "SEV"
-    | SEZ: "SEZ"
-    | SLEEP: "SLEEP"
-    | SPM x: "SPM"
-    | ST x: "ST"
-    | STS x: "STS"
-    | SUB x: "SUB"
-    | SUBI x: "SUBI"
-    | SWAP x: "SWAP"
-    | TST x: "TST"
-    | WDR: "WDR"
-    | XCH x: "XCH"
+val show/instruction insn = let
+   val show/ua mnemonic ua = case ua of
+      UA0: mnemonic
+    | UA1 a: mnemonic -++ show/unop a
+    | UA2 a: mnemonic -++ show/binop a
+    | UA3 a: mnemonic -++ show/ternop a
    end
-
-val pretty-mnemonic i = show/mnemonic i
+in
+   traverse show/ua insn
+end
 
 val show/register r =
    case r of
--- specifications/avr/avr.ml
+++ specifications/avr/avr.ml
@@ -1,4 +1,4 @@
-export = config-default decode typeof-opnd
+export = config-default decode
 
 val d ['bit:1'] = do
  rd <- query $rd;
@@ -553,39 +553,6 @@ type operand =
 type ternop = {first:operand,second:operand,third:operand}
 type binop = {first:operand,second:operand}
 type unop = {operand:operand}
-
-# Todo: Centralize
-# Operand types:
-# Immediate - 0
-# Register - 1
-# Memory - 2
-val typeof-opnd x i = let
-  val typeof-one o =
-    case o of
-       IMM a: 0
-     | REG a: 1
-     | REGHL a: 1
-     | REGIHL a: 1
-     | IOREG a: 1
-     | OPDI a: 2
-     | OPSE a: 2
-    end
-in
-  case (classify x) of
-     UNOP o: case i of
-        0: typeof-one o.operand
-     end
-   | BINOP o: case i of
-        0: typeof-one o.first
-      | 1: typeof-one o.second
-     end
-   | TERNOP o: case i of
-        0: typeof-one o.first
-      | 1: typeof-one o.second
-      | 2: typeof-one o.third
-     end
-  end
-end
 
 type instruction =
    ADC of binop
