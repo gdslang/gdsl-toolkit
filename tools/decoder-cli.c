1ef0e68d831ebc4a429210246af199fc72803d6e
Julian Kranz <kranzj@in.tum.de>
2014-08-22 19:17:39 +0200

    decoder-cli

--- tools/decoder-cli.c
+++ tools/decoder-cli.c
@@ -7,94 +7,75 @@
 #include <getopt.h>
 #include <readhex.h>
 #include <gdsl.h>
+#include <decoder_config.h>
 
-#ifdef GDSL_X86
-struct options {
-	char mode64;
-	char default_opnd_sz_32;
-};
-
-static char args_parse(int argc, char **argv, struct options *options) {
-	options->mode64 = 1;
-	options->default_opnd_sz_32 = 1;
-
-	while(1) {
-		char c = getopt(argc, argv, "md");
-		switch(c) {
-			case 'm': {
-				options->mode64 = 0;
-				break;
-			}
-			case 'd': {
-				options->default_opnd_sz_32 = 0;
-				break;
-			}
-			default: {
-				goto end;
-			}
-		}
-	}
-	end: ;
-
-	return 0;
+void print_help(state_t state, struct config_handlers handlers) {
+  printf("Usage: decoder-cli --help (for help)\n");
+  printf("Usage: decoder-cli (using default decoder options)\n");
+  printf("Usage: decoder-cli [-- decoder-options] (explicit decoder configuration)\n");
+  printf("Available decoder options:\n");
+  decoder_config_print_options(state, handlers);
 }
-#endif
 
 int main(int argc, char** argv) {
-	char retval = 0;
+  char retval = 0;
 
-#ifdef GDSL_X86
-	struct options options;
-	args_parse(argc, argv, &options);
+  state_t state = gdsl_init();
 
-	printf("Configuration:\n");
-	printf("\tmode64: %s\n", options.mode64 ? "true" : "false");
-	printf("\tdefault_opnd_sz = 32: %s\n", options.default_opnd_sz_32 ? "true" : "false");
-#endif
+  struct config_handlers handlers = { &gdsl_decoder_config, &gdsl_has_conf, &gdsl_conf_next, &gdsl_conf_short,
+      &gdsl_conf_long, &gdsl_conf_data };
+  int_t config = gdsl_config_default(state);
+  for(size_t i = 1; i < argc; ++i) {
+    if(!strcmp(argv[i], "--help")) {
+      print_help(state, handlers);
+      return 1;
+    } else if(!strcmp(argv[i], "--")) {
+      char success;
+      config = decoder_config_from_args(&success, state, handlers, argc - i - 1, argv + i + 1);
+      if(!success) {
+        print_help(state, handlers);
+        return 2;
+      }
+      break;
+    } else {
+      print_help(state, handlers);
+      return 3;
+    }
+  }
 
-	uint8_t *buffer;
-	size_t size = readhex_hex_read(stdin, &buffer);
+  uint8_t *buffer;
+  size_t size = readhex_hex_read(stdin, &buffer);
 
-	state_t state = gdsl_init();
-	gdsl_set_code(state, (char*)buffer, size, 0);
+  gdsl_set_code(state, (char*) buffer, size, 0);
 
-	if(setjmp(*gdsl_err_tgt(state))) {
-		fprintf(stderr, "decode failed: %s\n", gdsl_get_error_message(state));
-		retval = 1;
-		goto cleanup;
-	}
+  if(setjmp(*gdsl_err_tgt(state))) {
+    fprintf(stderr, "decode failed: %s\n", gdsl_get_error_message(state));
+    retval = 1;
+    goto cleanup;
+  }
 
-#ifdef GDSL_X86
-	int_t config = 0;
-	config |= gdsl_config_mode64(state)*options.mode64;
-	config |= gdsl_config_default_opnd_sz_32(state)*options.default_opnd_sz_32;
+  obj_t insn = gdsl_decode(state, config);
 
-	obj_t insn = gdsl_decode(state, config);
-#else
-	obj_t insn = gdsl_decode(state, gdsl_config_default(state));
-#endif
+  printf("[");
+  size_t decoded = gdsl_get_ip_offset(state);
+  for(size_t i = 0; i < decoded; ++i) {
+    if(i) printf(" ");
+    printf("%02x", buffer[i]);
+  }
+  printf("] ");
 
-	printf("[");
-	size_t decoded = gdsl_get_ip_offset(state);
-	for(size_t i = 0; i < decoded; ++i) {
-		if(i)
-			printf(" ");
-		printf("%02x", buffer[i]);
-	}
-	printf("] ");
-
-	string_t fmt = gdsl_merge_rope(state, gdsl_pretty(state, insn));
-	puts(fmt);
+  string_t fmt = gdsl_merge_rope(state, gdsl_pretty(state, insn));
+  puts(fmt);
 
   obj_t insn_asm = gdsl_generalize(state, insn);
   string_t asm_gen_str = gdsl_merge_rope(state, gdsl_asm_pretty(state, insn_asm));
-	puts(asm_gen_str);
+  puts(asm_gen_str);
 
-	cleanup:
+  cleanup:
 
-	gdsl_destroy(state);
-	free(buffer);
+  gdsl_destroy(state);
+  free(buffer);
 
-	return retval;
+  return retval;
 }
 
