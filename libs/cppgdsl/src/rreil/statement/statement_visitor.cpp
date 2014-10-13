5a323e0b9d1ff86d80b1f5ce3fa548549d9e7ec6
Julian Kranz <kranzj@in.tum.de>
2014-10-13 16:56:35 +0200

    cppgdsl: Bug fix

--- libs/cppgdsl/src/rreil/statement/statement_visitor.cpp
+++ libs/cppgdsl/src/rreil/statement/statement_visitor.cpp
@@ -11,56 +11,45 @@
 using namespace gdsl::rreil;
 
 void statement_visitor::visit(assign *a) {
-  if(assign_callback != NULL)
-    assign_callback(a);
-  _default(a);
+  if(assign_callback != NULL) assign_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(load *a) {
-  if(load_callback != NULL)
-    load_callback(a);
-  _default(a);
+  if(load_callback != NULL) load_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(store *a) {
-  if(store_callback != NULL)
-    store_callback(a);
-  _default(a);
+  if(store_callback != NULL) store_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(ite *a) {
-  if(ite_callback != NULL)
-    ite_callback(a);
-  _default(a);
+  if(ite_callback != NULL) ite_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(_while *a) {
-  if(_while_callback != NULL)
-    _while_callback(a);
-  _default(a);
+  if(_while_callback != NULL) _while_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(cbranch *a) {
-  if(cbranch_callback != NULL)
-    cbranch_callback(a);
-  _default(a);
+  if(cbranch_callback != NULL) cbranch_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(branch *a) {
-  if(branch_callback != NULL)
-    branch_callback(a);
-  _default(a);
+  if(branch_callback != NULL) branch_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(floating *a) {
-  if(floating_callback != NULL)
-    floating_callback(a);
-  _default(a);
+  if(floating_callback != NULL) floating_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(prim *a) {
-  if(prim_callback != NULL)
-    prim_callback(a);
-  _default(a);
+  if(prim_callback != NULL) prim_callback(a);
+  else _default(a);
 }
 void statement_visitor::visit(_throw *a) {
-  if(_throw_callback != NULL)
-    _throw_callback(a);
-  _default(a);
+  if(_throw_callback != NULL) _throw_callback(a);
+  else _default(a);
 }
 void statement_visitor::_default(statement *s) {
-  if(default_callback != NULL)
-    default_callback(s);
+  if(default_callback != NULL) default_callback(s);
 }
