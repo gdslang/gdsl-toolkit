d780f48ca885e2666a5699b778f39c4191d3f502
Michael Opitz <michael.w.opitz@gmail.com>
2014-10-16 13:37:38 +0200

    Added several opcodes to the pretty printer, which should simplify testing of the decoder.

--- specifications/arm7/arm7-pretty.ml
+++ specifications/arm7/arm7-pretty.ml
@@ -2,14 +2,44 @@
 
 export pretty : (insndata) -> rope
 
-val pretty insdata = show/instruction insdata.instruction
+val pretty insdata = show/instruction insdata.insn
 
 val show/instruction insn = show/mnemonic insn
 
 val show/mnemonic insn =
-   case insn of 
-      ADD m: "ADD"
+   case insn of
+      ADC m: "ADC"
+    | ADD m: "ADD"
+    | AND m: "AND"
+    | BIC m: "BIC"
+    | CMN m: "CMN"
+    | CMP m: "CMP"
+    | EOR m: "EOR"
+    | MOV m: "MOV"
+    | MVN m: "MVN"
+    | ORR m: "ORR"
+    | RSB m: "RSB"
+    | RSC m: "RSC"
+    | SBC m: "SBC"
     | SUB m: "SUB"
+    | TEQ m: "TEQ"
+    | TST m: "TST"
+    | MLA m: "MLA"
+    | MUL m: "MUL"
+    | SMLAL m: "SMLAL"
+    | SMULL m: "SMULL"
+    | UMLAL m: "UMULL"
+    | LDR m: "LDR"
+    | STR m: "STR"
+    | LDRH m: "LDRH"
+    | STRH m: "STRH"
+    | LDRSB m: "LDRSB"
+    | LDRSRH m: "LDRSRH"
+    | B m: "B"
+    | BL m: "BL"
+    | MRS m: "MRS"
+    | MSR m: "MSR"
+    | CLREX: "CLREX"
     | _: "UNKNOWN_MNEMONIC"
    end
 
