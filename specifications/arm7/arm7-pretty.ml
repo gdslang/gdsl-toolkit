9c6622553443333007f776c53c67cfd3866570f4
Michael Opitz <michael.w.opitz@gmail.com>
2014-10-23 18:02:53 +0200

    Improved pretty printing.

--- specifications/arm7/arm7-pretty.ml
+++ specifications/arm7/arm7-pretty.ml
@@ -6,57 +6,74 @@ val pretty insdata = show/instruction insdata.insn
 
 val -++ a b = a +++ " " +++ b
 
-val show/instruction insn = show/mnemonic insn -++ show/registers insn
+type instruction_class =
+    NONE
+  | BR of br      # branch
+  | DP of dp      # data processing
+  | LSS of ls     # load/store single
+  | LSM of lsm    # load/store multiple
+  | ML of mul     # multiply
+  | MLL of mull   # mulitply long
 
-val show/mnemonic insn =
-  case insn of
-      ADC m: "ADC"
-    | ADD m: "ADD"
-    | AND m: "AND"
-    | BIC m: "BIC"
-    | CMN m: "CMN"
-    | CMP m: "CMP"
-    | EOR m: "EOR"
-    | MOV m: "MOV"
-    | MVN m: "MVN"
-    | ORR m: "ORR"
-    | RSB m: "RSB"
-    | RSC m: "RSC"
-    | SBC m: "SBC"
-    | SUB m: "SUB"
-    | TEQ m: "TEQ"
-    | TST m: "TST"
-    | MLA m: "MLA"
-    | MUL m: "MUL"
-    | SMLAL m: "SMLAL"
-    | SMULL m: "SMULL"
-    | UMLAL m: "UMULL"
-    | STR m: "STR"
-    | LDR m: "LDR"
-    | LDRH m: "LDRH"
-    | STRH m: "STRH"
-    | LDRSB m: "LDRSB"
-    | LDRSRH m: "LDRSRH"
-    | LDM m: "LDM"
-    | LDMDA m: "LDMDA"
-    | LDMDB m: "LDMDB"
-    | LDMIB m: "LDMIB"
-    | POP m: "POP"
-    | STM m: "STM"
-    | STMDA m: "STMDA"
-    | STMDB m: "STMDB"
-    | STMIB m: "STMIB"
-    | PUSH m: "PUSH"
-    | B m: "B"
-    | BL m: "BL"
-    | BLX m: "BLX"
-    | BX m: "BX"
-    | BXJ m: "BXJ"
-    | MRS m: "MRS"
-    | MSR m: "MSR"
-    | NOP m: "NOP"
-    | _: "???"
+val show/instruction insn = let
+  val show/i mnemonic i = case i of
+      DP c: mnemonic +++ show/dp c
+    | LSS c: mnemonic +++ show/ls c
+    | LSM c: mnemonic +++ show/lsm c
+    | NONE: mnemonic
   end
+in
+  traverse show/i insn
+end
+
+val traverse f insn =
+  case insn of
+      ADC a: f "ADC" (DP a)
+    | ADD a: f "ADD" (DP a)
+    | AND a: f "AND" (DP a)
+    | BIC a: f "BIC" (DP a)
+    | CMN a: f "CMN" (DP a)
+    | CMP a: f "CMP" (DP a)
+    | EOR a: f "EOR" (DP a)
+    | MOV a: f "MOV" (DP a)
+    | MVN a: f "MVN" (DP a)
+    | ORR a: f "ORR" (DP a)
+    | RSB a: f "RSB" (DP a)
+    | RSC a: f "RSC" (DP a)
+    | SBC a: f "SBC" (DP a)
+    | SUB a: f "SUB" (DP a)
+    | TEQ a: f "TEQ" (DP a)
+    | TST a: f "TST" (DP a)
+    | LDR b: f "LDR" (LSS b)
+    | LDRB b: f "LDRB" (LSS b)
+    | LDRD b: f "LDRD" (LSS b)
+    | LDRH b: f "LDRH" (LSS b)
+    | LDRSB b: f "LDRSB" (LSS b)
+    | LDRSH b: f "LDRSH" (LSS b)
+    | LDRSRH b: f "LDRSRH" (LSS b)
+    | STR b: f "STR" (LSS b)
+    | STRB b: f "STRB" (LSS b)
+    | STRD b: f "STRD" (LSS b)
+    | STRH b: f "STRH" (LSS b)
+    | LDM c: f "LDM" (LSM c)
+    | LDMDA c: f "LDMCA" (LSM c)
+    | LDMDB c: f "LDMDB" (LSM c)
+    | LDMIB c: f "LDMIB" (LSM c)
+    | POP c: f "POP" (LSM c)
+    | STM c: f "STM" (LSM c)
+    | STMDA c: f "STM" (LSM c)
+    | STMDB c: f "STMDB" (LSM c)
+    | STMIB c: f "STMIB" (LSM c)
+    | PUSH c: f "PUSH" (LSM c)
+    | _: f "???" NONE
+  end
+
+val show/dp insn = show/s insn +++ show/condition insn.cond -++ show/register insn.rn +++ "," -++ show/register insn.rd +++ "," -++ show/operand insn.op2
+
+val show/s insn = if insn.s then "S" else ""
+
+val show/ls insn = ""
+val show/lsm insn = ""
 
 val show/condition cond =
   case cond of
@@ -98,11 +115,24 @@ val show/register reg =
     | _: "??"
   end
 
-val show/registers insn =
-  case insn of
-      PUSH i: "{" +++ show/reglist i.register_list +++ "}"
-    | POP i: "{" +++ show/reglist i.register_list +++ "}"
-    | _ : ""
+val show/immediate imm =
+  case imm of
+      INT i: show-int i
+    | IMM5 i: show-int (zx i)
+    | IMM8 i: show-int (zx i)
+    | IMM12 i: show-int (zx i)
+    | IMM24 i: show-int (zx i)
+    | MODIMM i: "#" +++ show-int(zx i.byte) +++ "," -++ "#" +++ show-int(zx i.rot)
+    | _: "???"
+  end
+
+val show/operand op =
+  case op of
+      IMMEDIATE i: show/immediate i
+    | REGISTER r: show/register r
+    | REGISTER_LIST l: "{" +++ show/reglist l +++ "}"
+    | SHIFTED_REGISTER s: 
+    | _: "???"
   end
 
 val show/reglist reglist =
