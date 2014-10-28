6ac29c7c256faa03c27c9493be30b58cbf4f4df1
Michael Opitz <michael.w.opitz@gmail.com>
2014-10-28 17:25:28 +0100

    Added a very basic B (branch) instruction translator. The condition code is currently still ignored.

--- specifications/arm7/arm7-rreil-translator.ml
+++ specifications/arm7/arm7-rreil-translator.ml
@@ -19,24 +19,46 @@ end
 
 val relative-next stmts = let
   val is_sem_ip x = case x of
-      Sem_IP: '1'
+      Sem_PC: '1'
     | _: '0'
   end
 in
   relative-next-generic is_sem_ip stmts
 end
 
+type signedness =
+    Signed
+  | Unsigned
+
+val rval sn x = let
+  val from-vec sn vec =
+    case sn of
+        Signed: SEM_LIN_IMM {const=sx vec}
+      | Unsigned: SEM_LIN_IMM {const=zx vec}
+    end
+  val from-imm sn imm =
+    case imm of
+        IMMi i: SEM_LIN_IMM {const=i}
+    end
+in
+  case x of
+      REGISTER r: return (var (semantic-register-of r))
+    | IMMEDIATE i: return (from-imm sn i)
+  end
+end
+
 
 val semantics insn =
   case insn of
-      ADC x: sem-adc x
-    | AND x: sem-and x
+      B x: sem-b x
   end
 
-val sem-adc x = do
-  return 0
+val get-pc = return (semantic-register-of R15)
+val get-sp = return (semantic-register-of R13)
+
+val sem-b x = do
+  pc <- get-pc;
+  offset <- rval Unsigned x.label;
+  jump (address pc.size offset)
 end
 
-val sem-and x = do
-  return 0
-end
