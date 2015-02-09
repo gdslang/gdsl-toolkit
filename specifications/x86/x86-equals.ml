ac2492e4f5dc82f3ae7a771f505f3c1a91b98553
Julian Kranz <kranzj@in.tum.de>
2015-02-09 22:33:20 +0100

    Equality of x86 operands

--- specifications/x86/x86-equals.ml
+++ specifications/x86/x86-equals.ml
@@ -3,10 +3,24 @@ val equals-opnd oa ob = case oa of
  | IMM16 ia: case ob of IMM16 ib: ia.address === ib.address and ia.imm == ib.imm | _: '0' end
  | IMM32 ia: case ob of IMM32 ib: ia.address === ib.address and ia.imm == ib.imm | _: '0' end
  | IMM64 ia: case ob of IMM64 ib: ia.address === ib.address and ia.imm == ib.imm | _: '0' end
- | REG ra: case ob of REG rb: (index ra) === (index rb) | _: '0' end
- | MEM ma: case ob of MEM mb: (ma.sz === mb.sz) and (ma.psz === mb.psz) and
-     (equals-opnd ma.opnd mb.opnd) | _: '0' end #todo: seg_override
- | _: '0'
-# | X86_SUM s: ...
-# | X86_SCALE s: ...
+ | REG ra: case ob of REG rb: equals-register ra rb | _: '0' end
+ | MEM ma: case ob of MEM mb: equals-mem ma mb | _: '0' end
+ | X86_SUM sa: case ob of X86_SUM sb: (equals-opnd sa.a sb.a) and (equals-opnd
+   sa.b sb.b) | _: '0' end
+ | X86_SCALE sa: case ob of X86_SCALE sb: (sa.imm == sb.imm) and (equals-opnd
+   sa.opnd sb.opnd) | _: '0' end
+end
+
+val equals-register ra rb = (index ra) === (index rb)
+
+val equals-mem ma mb =
+  (ma.sz === mb.sz) and
+  (ma.psz === mb.psz) and
+  (equals-seg-over ma.segment mb.segment) and
+  (equals-opnd ma.opnd mb.opnd)
+
+val equals-seg-over sa sb = case sa of
+   SEG_NONE: case sb of SEG_NONE: '1' | _: '0' end
+ | SEG_OVERRIDE ra: case sb of SEG_OVERRIDE rb: equals-register ra rb | _: '0'
+   end
 end
