5c752bd92e78e35965ddedb8aac43331c56637fe
Julian Kranz <kranzj@google.com>
2016-10-06 16:41:25 +0200

    Multiplexing library

--- libs/gdsl-multiplex/include/gdsl_generic.h
+++ libs/gdsl-multiplex/include/gdsl_generic.h
@@ -1,8 +1,6 @@
 /*
- * gdsl_generic.h
- *
- *  Created on: Apr 29, 2014
- *      Author: Julian Kranz
+ * This code is partly generated and represents the interface that
+ * the different instructions decoders, etc. expose.
  */
 
 #pragma once
@@ -13,9 +11,7 @@
  * RReil defintions
  */
 
-enum id_shared {
-  ID_FLOATING_FLAGS = 0
-};
+enum id_shared { ID_FLOATING_FLAGS = 0 };
 
 enum branch_hint {
   BRANCH_HINT_JUMP = 0,
@@ -23,15 +19,9 @@ enum branch_hint {
   BRANCH_HINT_RET = 2
 };
 
-enum exception {
-  EXCEPTION_DIVISION_BY_ZERO = 0
-};
+enum exception { EXCEPTION_DIVISION_BY_ZERO = 0 };
 
-enum flop {
-  FLOP_FADD = 0,
-  FLOP_FSUB = 1,
-  FLOP_FMUL = 2
-};
+enum flop { FLOP_FADD = 0, FLOP_FSUB = 1, FLOP_FMUL = 2 };
 
 enum optimization_configuration {
   PRESERVATION_EVERYWHERE = 1,
@@ -45,7 +35,7 @@ enum optimization_configuration {
 #ifndef __GDSL_RUNTIME_H
 
 struct state {
-  void *userdata; /* a pointer to arbitary data */
+  void* userdata; /* a pointer to arbitary data */
 };
 
 /* data types used in decoder programs */
@@ -68,98 +58,98 @@ typedef struct vec vec_t;
 typedef int_t con_tag_t;
 
 typedef struct {
-  obj_t (*branch_hint_)(state_t,int_t);
+  obj_t (*branch_hint_)(state_t, int_t);
 } unboxed_branch_hint_callbacks_t;
 typedef unboxed_branch_hint_callbacks_t* branch_hint_callbacks_t;
 typedef struct {
-  obj_t (*sem_address_)(state_t,int_t,obj_t);
+  obj_t (*sem_address_)(state_t, int_t, obj_t);
 } unboxed_sem_address_callbacks_t;
 typedef unboxed_sem_address_callbacks_t* sem_address_callbacks_t;
 typedef struct {
-  obj_t (*arch)(state_t,string_t);
-  obj_t (*shared)(state_t,int_t);
+  obj_t (*arch)(state_t, string_t);
+  obj_t (*shared)(state_t, int_t);
 } unboxed_sem_exception_callbacks_t;
 typedef unboxed_sem_exception_callbacks_t* sem_exception_callbacks_t;
 typedef struct {
-  obj_t (*sem_and)(state_t,obj_t,obj_t);
-  obj_t (*sem_div)(state_t,obj_t,obj_t);
-  obj_t (*sem_divs)(state_t,obj_t,obj_t);
-  obj_t (*sem_mod)(state_t,obj_t,obj_t);
-  obj_t (*sem_mods)(state_t,obj_t,obj_t);
-  obj_t (*sem_mul)(state_t,obj_t,obj_t);
-  obj_t (*sem_or)(state_t,obj_t,obj_t);
-  obj_t (*sem_sexpr)(state_t,obj_t);
-  obj_t (*sem_shl)(state_t,obj_t,obj_t);
-  obj_t (*sem_shr)(state_t,obj_t,obj_t);
-  obj_t (*sem_shrs)(state_t,obj_t,obj_t);
-  obj_t (*sem_sx)(state_t,int_t,obj_t);
-  obj_t (*sem_xor)(state_t,obj_t,obj_t);
-  obj_t (*sem_zx)(state_t,int_t,obj_t);
+  obj_t (*sem_and)(state_t, obj_t, obj_t);
+  obj_t (*sem_div)(state_t, obj_t, obj_t);
+  obj_t (*sem_divs)(state_t, obj_t, obj_t);
+  obj_t (*sem_mod)(state_t, obj_t, obj_t);
+  obj_t (*sem_mods)(state_t, obj_t, obj_t);
+  obj_t (*sem_mul)(state_t, obj_t, obj_t);
+  obj_t (*sem_or)(state_t, obj_t, obj_t);
+  obj_t (*sem_sexpr)(state_t, obj_t);
+  obj_t (*sem_shl)(state_t, obj_t, obj_t);
+  obj_t (*sem_shr)(state_t, obj_t, obj_t);
+  obj_t (*sem_shrs)(state_t, obj_t, obj_t);
+  obj_t (*sem_sx)(state_t, int_t, obj_t);
+  obj_t (*sem_xor)(state_t, obj_t, obj_t);
+  obj_t (*sem_zx)(state_t, int_t, obj_t);
 } unboxed_sem_expr_callbacks_t;
 typedef unboxed_sem_expr_callbacks_t* sem_expr_callbacks_t;
 typedef struct {
-  obj_t (*sem_cmpeq)(state_t,obj_t,obj_t);
-  obj_t (*sem_cmples)(state_t,obj_t,obj_t);
-  obj_t (*sem_cmpleu)(state_t,obj_t,obj_t);
-  obj_t (*sem_cmplts)(state_t,obj_t,obj_t);
-  obj_t (*sem_cmpltu)(state_t,obj_t,obj_t);
-  obj_t (*sem_cmpneq)(state_t,obj_t,obj_t);
+  obj_t (*sem_cmpeq)(state_t, obj_t, obj_t);
+  obj_t (*sem_cmples)(state_t, obj_t, obj_t);
+  obj_t (*sem_cmpleu)(state_t, obj_t, obj_t);
+  obj_t (*sem_cmplts)(state_t, obj_t, obj_t);
+  obj_t (*sem_cmpltu)(state_t, obj_t, obj_t);
+  obj_t (*sem_cmpneq)(state_t, obj_t, obj_t);
 } unboxed_sem_expr_cmp_callbacks_t;
 typedef unboxed_sem_expr_cmp_callbacks_t* sem_expr_cmp_callbacks_t;
 typedef struct {
-  obj_t (*sem_flop_)(state_t,int_t);
+  obj_t (*sem_flop_)(state_t, int_t);
 } unboxed_sem_flop_callbacks_t;
 typedef unboxed_sem_flop_callbacks_t* sem_flop_callbacks_t;
 typedef struct {
-  obj_t (*arch)(state_t,string_t);
-  obj_t (*shared)(state_t,int_t);
-  obj_t (*virt_o)(state_t,int_t);
-  obj_t (*virt_t)(state_t,int_t);
+  obj_t (*arch)(state_t, string_t);
+  obj_t (*shared)(state_t, int_t);
+  obj_t (*virt_o)(state_t, int_t);
+  obj_t (*virt_t)(state_t, int_t);
 } unboxed_sem_id_callbacks_t;
 typedef unboxed_sem_id_callbacks_t* sem_id_callbacks_t;
 typedef struct {
-  obj_t (*sem_lin_add)(state_t,obj_t,obj_t);
-  obj_t (*sem_lin_imm)(state_t,int_t);
-  obj_t (*sem_lin_scale)(state_t,int_t,obj_t);
-  obj_t (*sem_lin_sub)(state_t,obj_t,obj_t);
-  obj_t (*sem_lin_var)(state_t,obj_t);
+  obj_t (*sem_lin_add)(state_t, obj_t, obj_t);
+  obj_t (*sem_lin_imm)(state_t, int_t);
+  obj_t (*sem_lin_scale)(state_t, int_t, obj_t);
+  obj_t (*sem_lin_sub)(state_t, obj_t, obj_t);
+  obj_t (*sem_lin_var)(state_t, obj_t);
 } unboxed_sem_linear_callbacks_t;
 typedef unboxed_sem_linear_callbacks_t* sem_linear_callbacks_t;
 typedef struct {
   obj_t (*sem_sexpr_arb)(state_t);
-  obj_t (*sem_sexpr_cmp)(state_t,int_t,obj_t);
-  obj_t (*sem_sexpr_lin)(state_t,obj_t);
+  obj_t (*sem_sexpr_cmp)(state_t, int_t, obj_t);
+  obj_t (*sem_sexpr_lin)(state_t, obj_t);
 } unboxed_sem_sexpr_callbacks_t;
 typedef unboxed_sem_sexpr_callbacks_t* sem_sexpr_callbacks_t;
 typedef struct {
-  obj_t (*sem_assign)(state_t,int_t,obj_t,obj_t);
-  obj_t (*sem_branch)(state_t,obj_t,obj_t);
-  obj_t (*sem_cbranch)(state_t,obj_t,obj_t,obj_t);
-  obj_t (*sem_flop)(state_t,obj_t,obj_t,obj_t,obj_t);
-  obj_t (*sem_ite)(state_t,obj_t,obj_t,obj_t);
-  obj_t (*sem_load)(state_t,int_t,obj_t,obj_t);
-  obj_t (*sem_prim)(state_t,string_t,obj_t,obj_t);
-  obj_t (*sem_store)(state_t,int_t,obj_t,obj_t);
-  obj_t (*sem_throw)(state_t,obj_t);
-  obj_t (*sem_while)(state_t,obj_t,obj_t);
+  obj_t (*sem_assign)(state_t, int_t, obj_t, obj_t);
+  obj_t (*sem_branch)(state_t, obj_t, obj_t);
+  obj_t (*sem_cbranch)(state_t, obj_t, obj_t, obj_t);
+  obj_t (*sem_flop)(state_t, obj_t, obj_t, obj_t, obj_t);
+  obj_t (*sem_ite)(state_t, obj_t, obj_t, obj_t);
+  obj_t (*sem_load)(state_t, int_t, obj_t, obj_t);
+  obj_t (*sem_prim)(state_t, string_t, obj_t, obj_t);
+  obj_t (*sem_store)(state_t, int_t, obj_t, obj_t);
+  obj_t (*sem_throw)(state_t, obj_t);
+  obj_t (*sem_while)(state_t, obj_t, obj_t);
 } unboxed_sem_stmt_callbacks_t;
 typedef unboxed_sem_stmt_callbacks_t* sem_stmt_callbacks_t;
 typedef struct {
   obj_t (*sem_stmt_list_init)(state_t);
-  obj_t (*sem_stmt_list_next)(state_t,obj_t,obj_t);
+  obj_t (*sem_stmt_list_next)(state_t, obj_t, obj_t);
 } unboxed_sem_stmt_list_callbacks_t;
 typedef unboxed_sem_stmt_list_callbacks_t* sem_stmt_list_callbacks_t;
 typedef struct {
-  obj_t (*sem_var_)(state_t,obj_t,int_t);
+  obj_t (*sem_var_)(state_t, obj_t, int_t);
 } unboxed_sem_var_callbacks_t;
 typedef unboxed_sem_var_callbacks_t* sem_var_callbacks_t;
 typedef struct {
-  obj_t (*sem_varl_)(state_t,obj_t,int_t,int_t);
+  obj_t (*sem_varl_)(state_t, obj_t, int_t, int_t);
 } unboxed_sem_varl_callbacks_t;
 typedef unboxed_sem_varl_callbacks_t* sem_varl_callbacks_t;
 typedef struct {
   obj_t (*sem_varl_list_init)(state_t);
-  obj_t (*sem_varl_list_next)(state_t,obj_t,obj_t);
+  obj_t (*sem_varl_list_next)(state_t, obj_t, obj_t);
 } unboxed_sem_varl_list_callbacks_t;
 typedef unboxed_sem_varl_list_callbacks_t* sem_varl_list_callbacks_t;
 typedef struct {
@@ -215,39 +205,40 @@ typedef struct {
 } unboxed_asm_insn_t;
 typedef unboxed_asm_insn_t* asm_insn_t;
 typedef struct {
-  obj_t (*ann_string)(state_t,string_t);
-  obj_t (*function)(state_t,string_t,obj_t);
-  obj_t (*opnd)(state_t,string_t,obj_t);
+  obj_t (*ann_string)(state_t, string_t);
+  obj_t (*function)(state_t, string_t, obj_t);
+  obj_t (*opnd)(state_t, string_t, obj_t);
 } unboxed_asm_annotation_callbacks_t;
 typedef unboxed_asm_annotation_callbacks_t* asm_annotation_callbacks_t;
 typedef struct {
-  obj_t (*annotation_list_next)(state_t,obj_t,obj_t);
+  obj_t (*annotation_list_next)(state_t, obj_t, obj_t);
   obj_t (*init)(state_t);
 } unboxed_asm_annotation_list_callbacks_t;
-typedef unboxed_asm_annotation_list_callbacks_t* asm_annotation_list_callbacks_t;
+typedef unboxed_asm_annotation_list_callbacks_t*
+    asm_annotation_list_callbacks_t;
 typedef struct {
-  obj_t (*sz)(state_t,int_t);
-  obj_t (*sz_o)(state_t,int_t,int_t);
+  obj_t (*sz)(state_t, int_t);
+  obj_t (*sz_o)(state_t, int_t, int_t);
 } unboxed_asm_boundary_callbacks_t;
 typedef unboxed_asm_boundary_callbacks_t* asm_boundary_callbacks_t;
 typedef struct {
-  obj_t (*annotated)(state_t,obj_t,obj_t);
-  obj_t (*bounded)(state_t,obj_t,obj_t);
-  obj_t (*composite)(state_t,obj_t);
-  obj_t (*imm)(state_t,int_t);
-  obj_t (*memory)(state_t,obj_t);
-  obj_t (*opnd_register)(state_t,string_t);
-  obj_t (*post_op)(state_t,obj_t,obj_t);
-  obj_t (*pre_op)(state_t,obj_t,obj_t);
-  obj_t (*rel)(state_t,obj_t);
-  obj_t (*scale)(state_t,int_t,obj_t);
-  obj_t (*sign)(state_t,obj_t,obj_t);
-  obj_t (*sum)(state_t,obj_t,obj_t);
+  obj_t (*annotated)(state_t, obj_t, obj_t);
+  obj_t (*bounded)(state_t, obj_t, obj_t);
+  obj_t (*composite)(state_t, obj_t);
+  obj_t (*imm)(state_t, int_t);
+  obj_t (*memory)(state_t, obj_t);
+  obj_t (*opnd_register)(state_t, string_t);
+  obj_t (*post_op)(state_t, obj_t, obj_t);
+  obj_t (*pre_op)(state_t, obj_t, obj_t);
+  obj_t (*rel)(state_t, obj_t);
+  obj_t (*scale)(state_t, int_t, obj_t);
+  obj_t (*sign)(state_t, obj_t, obj_t);
+  obj_t (*sum)(state_t, obj_t, obj_t);
 } unboxed_asm_opnd_callbacks_t;
 typedef unboxed_asm_opnd_callbacks_t* asm_opnd_callbacks_t;
 typedef struct {
   obj_t (*init)(state_t);
-  obj_t (*opnd_list_next)(state_t,obj_t,obj_t);
+  obj_t (*opnd_list_next)(state_t, obj_t, obj_t);
 } unboxed_asm_opnd_list_callbacks_t;
 typedef unboxed_asm_opnd_list_callbacks_t* asm_opnd_list_callbacks_t;
 typedef struct {
@@ -259,7 +250,7 @@ typedef struct {
   asm_annotation_callbacks_t annotation;
   asm_annotation_list_callbacks_t annotation_list;
   asm_boundary_callbacks_t boundary;
-  obj_t (*insn)(state_t,int_t,string_t,obj_t,obj_t);
+  obj_t (*insn)(state_t, int_t, string_t, obj_t, obj_t);
   asm_opnd_callbacks_t opnd;
   asm_opnd_list_callbacks_t opnd_list;
   asm_signedness_callbacks_t signedness;
--- libs/gdsl-multiplex/include/gdsl_multiplex.h
+++ libs/gdsl-multiplex/include/gdsl_multiplex.h
@@ -1,29 +1,53 @@
 /*
- * gdsl_multiplex.h
- *
- *  Created on: Sep 11, 2013
- *      Author: jucs
+ * This file contains all definitions needed to generically load GDSL
+ * front-ends and interface with them. In specific, it contains
+ * data structures that store a callback for each exported function of GDSL.
  */
 
 #ifndef GDSL_MULTIPLEX_H_
 #define GDSL_MULTIPLEX_H_
 
-#include <gdsl_generic.h>
 #include <setjmp.h>
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <gdsl_generic.h>
+
+/*
+ * This structure contains everything required to
+ * identify a front-end.
+ */
 struct frontend_desc {
-  const char *name;
-  const char *ext;
+  /*
+   * The name of the front-end
+   */
+  const char* name;
+  /*
+   * The file extension of the front-end library
+   */
+  const char* ext;
 };
 
+/*
+ * This structure represents a loaded front-end. It stores
+ * a callback for each API function. The structure is broken
+ * up into parts that are generic, regard the decoder, or belong
+ * to the translator.
+ *
+ * The frontend also stores a pointer to the loaded shared object.
+ * This way, the front-end can be closed after finishing with it.
+ *
+ * Documentation for the callbacks can be found here:
+ * https://github.com/gdslang/gdsl-toolkit/wiki/InterfacingC
+ * https://github.com/gdslang/gdsl-toolkit/wiki/DecoderTranslatorAPI
+ */
 struct frontend {
   struct {
     state_t (*init)();
-    void (*set_code)(state_t state, unsigned char *buffer, uint64_t size, uint64_t base);
-    char (*seek)(state_t state, int_t ip);
-    jmp_buf *(*err_tgt)(state_t s);
+    void (*set_code)(state_t state, const unsigned char* buffer, size_t size,
+                     uint64_t base);
+    char (*seek)(state_t state, uint64_t ip);
+    jmp_buf* (*err_tgt)(state_t s);
     string_t (*merge_rope)(state_t s, obj_t rope);
     char* (*get_error_message)(state_t s);
     uint64_t (*get_ip)(state_t s);
@@ -50,26 +74,82 @@ struct frontend {
     obj_t (*pretty)(state_t state, obj_t rreil);
     obj_t (*pretty_arch_id)(state_t state, obj_t id);
     obj_t (*pretty_arch_exception)(state_t state, obj_t id);
-    obj_t (*rreil_convert_sem_stmt_list)(state_t s, callbacks_t cbs, obj_t stmts);
+    obj_t (*rreil_convert_sem_stmt_list)(state_t s, callbacks_t cbs,
+                                         obj_t stmts);
     obj_t (*optimization_config)(state_t state);
-    opt_result_t (*decode_translate_block_optimized)(state_t state, int_t config, int_t limit, int_t pres);
-    obj_t (*traverse_insn_list)(state_t state, obj_t insn_list, obj_t insns_init,
-        obj_t (*insn_cb)(state_t, obj_t, obj_t));
+    opt_result_t (*decode_translate_block_optimized)(state_t state,
+                                                     int_t config, int_t limit,
+                                                     int_t pres);
+    obj_t (*traverse_insn_list)(state_t state, obj_t insn_list,
+                                obj_t insns_init,
+                                obj_t (*insn_cb)(state_t, obj_t, obj_t));
   } translator;
 
-  void *dl;
+  void* dl;
 };
 
+/*
+ * The following lines define error codes used by the functions
+ * below.
+ */
 #define GDSL_MULTIPLEX_ERROR_NONE 0
 #define GDSL_MULTIPLEX_ERROR_FRONTENDS_PATH_NOT_SET 1
 #define GDSL_MULTIPLEX_ERROR_UNABLE_TO_OPEN 2
 #define GDSL_MULTIPLEX_ERROR_SYMBOL_NOT_FOUND 3
 
-extern size_t gdsl_multiplex_frontends_list(struct frontend_desc **descs);
-extern size_t gdsl_multiplex_frontends_list_with_base(struct frontend_desc **descs, char const *base);
-extern char gdsl_multiplex_frontend_get_by_desc(struct frontend *frontend, struct frontend_desc desc);
-extern char gdsl_multiplex_frontend_get_by_lib_name(struct frontend *frontend, char const *name);
-extern void gdsl_multiplex_descs_free(struct frontend_desc *descs, size_t descs_length);
-extern void gdsl_multiplex_frontend_close(struct frontend *frontend);
+/*
+ * This function lists available front-ends. The number of front-ends
+ * is returned while the front-ends descriptors are returned via the
+ * first argument.
+ *
+ * Important: This is a stub only. This function returns a fixed front-end.
+ */
+extern size_t gdsl_multiplex_frontends_list(struct frontend_desc** descs);
+
+/*
+ * This function lists front-ends (see above) given a search path
+ * to look them up.
+ *
+ * Important: This is a stub only. This function returns a fixed front-end.
+ */
+extern size_t gdsl_multiplex_frontends_list_with_base(
+    struct frontend_desc** descs, char const* base);
+
+/*
+ * This function opens a front-end and returns whether opening was
+ * successful.
+ *
+ * Important: This is a stub only. This function always returns a front-end
+ * where all callbacks point to linked functions. There is no opening
+ * going on.
+ */
+extern int gdsl_multiplex_frontend_get_by_desc(struct frontend* frontend,
+                                               struct frontend_desc desc);
+
+/*
+ * This function opens a front-end specified by its library name and returns
+ * whether opening was successful.
+ *
+ * Important: This is a stub only. This function always returns a front-end
+ * where all callbacks point to linked functions. There is no opening
+ * going on.
+ */
+extern int gdsl_multiplex_frontend_get_by_lib_name(struct frontend* frontend,
+                                                   char const* name);
+
+/*
+ * This function frees all resources associated with a list of
+ * front-end descriptors. It must only be used to free memory
+ * allocated by one of the list* functions from above.
+ */
+extern void gdsl_multiplex_descs_free(struct frontend_desc* descs,
+                                      size_t descs_length);
+
+/*
+ * This function closes a front-end.
+ *
+ * Important: This is a stub only and does nothing.
+ */
+extern void gdsl_multiplex_frontend_close(struct frontend* frontend);
 
 #endif /* GDSL_MULTIPLEX_H_ */
