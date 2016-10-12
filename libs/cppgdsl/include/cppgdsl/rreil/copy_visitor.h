446a58613de6c5da82d0aed97b0059de218bde72
Julian Kranz <kranzj@in.tum.de>
2016-10-12 21:32:46 +0200

    copy_visitor

---
+++ libs/cppgdsl/include/cppgdsl/rreil/copy_visitor.h
@@ -0,0 +1,247 @@
+#pragma once
+
+#include <vector>
+
+#include <cppgdsl/rreil/branch_hint.h>
+#include <cppgdsl/rreil/exception/shared_exception.h>
+#include <cppgdsl/rreil/expr_cmp/cmp_op.h>
+#include <cppgdsl/rreil/expr/binop_op.h>
+#include <cppgdsl/rreil/expr/expr_ext.h>
+#include <cppgdsl/rreil/flop.h>
+#include <cppgdsl/rreil/id/shared_id.h>
+#include <cppgdsl/rreil/linear/binop_lin_op.h>
+#include <cppgdsl/rreil/visitor.h>
+
+extern "C" {
+#include <gdsl_generic.h>
+}
+
+namespace gdsl {
+namespace rreil {
+
+class linear;
+class expr;
+class sexpr;
+class statement;
+class id;
+class exception;
+
+class variable;
+class variable_limited;
+class address;
+class expr_cmp;
+
+class copy_visitor : public visitor {
+ public:
+  using variable_ctor_t =
+      std::function<std::unique_ptr<variable>(std::unique_ptr<id>, int_t)>;
+  using variable_limited_ctor_t =
+      std::function<std::unique_ptr<variable_limited>(std::unique_ptr<id>,
+                                                      int_t, int_t)>;
+  using address_ctor_t =
+      std::function<std::unique_ptr<address>(int_t, std::unique_ptr<linear>)>;
+  using expr_cmp_ctor_t = std::function<std::unique_ptr<expr_cmp>(
+      cmp_op, std::unique_ptr<linear>, std::unique_ptr<linear>)>;
+  using expr_binop_ctor_t = std::function<std::unique_ptr<expr>(
+      binop_op, std::unique_ptr<linear>, std::unique_ptr<linear>)>;
+  using expr_ext_ctor_t = std::function<std::unique_ptr<expr>(
+      ext_op, int_t, std::unique_ptr<linear>)>;
+  using expr_sexpr_ctor_t =
+      std::function<std::unique_ptr<expr>(std::unique_ptr<sexpr>)>;
+  using arch_id_ctor_t = std::function<std::unique_ptr<id>(std::string)>;
+  using shared_id_ctor_t = std::function<std::unique_ptr<id>(shared_id_type)>;
+  using _virtual_ctor_t = std::function<std::unique_ptr<id>(int_t, bool)>;
+  using lin_binop_ctor_t = std::function<std::unique_ptr<linear>(
+      binop_lin_op, std::unique_ptr<linear>, std::unique_ptr<linear>)>;
+  using lin_imm_ctor_t = std::function<std::unique_ptr<linear>(int_t)>;
+  using lin_scale_ctor_t =
+      std::function<std::unique_ptr<linear>(int_t, std::unique_ptr<linear>)>;
+  using lin_var_ctor_t =
+      std::function<std::unique_ptr<linear>(std::unique_ptr<variable>)>;
+  using arbitrary_ctor_t = std::function<std::unique_ptr<sexpr>()>;
+  using sexpr_cmp_ctor_t =
+      std::function<std::unique_ptr<sexpr>(int_t, std::unique_ptr<expr_cmp>)>;
+  using sexpr_lin_ctor_t =
+      std::function<std::unique_ptr<sexpr>(std::unique_ptr<linear>)>;
+  using assign_ctor_t = std::function<std::unique_ptr<statement>(
+      int_t, std::unique_ptr<variable>, std::unique_ptr<expr>)>;
+  using load_ctor_t = std::function<std::unique_ptr<statement>(
+      int_t, std::unique_ptr<variable>, std::unique_ptr<address>)>;
+  using store_ctor_t = std::function<std::unique_ptr<statement>(
+      int_t, std::unique_ptr<address>, std::unique_ptr<linear>)>;
+  using ite_ctor_t = std::function<std::unique_ptr<statement>(
+      std::unique_ptr<sexpr>, std::vector<std::unique_ptr<statement>>,
+      std::vector<std::unique_ptr<statement>>)>;
+  using _while_ctor_t = std::function<std::unique_ptr<statement>(
+      std::unique_ptr<sexpr>, std::vector<std::unique_ptr<statement>>)>;
+  using cbranch_ctor_t = std::function<std::unique_ptr<statement>(
+      std::unique_ptr<sexpr>, std::unique_ptr<address>,
+      std::unique_ptr<address>)>;
+  using branch_ctor_t = std::function<std::unique_ptr<statement>(
+      std::unique_ptr<address>, ::gdsl::rreil::branch_hint)>;
+  using floating_ctor_t = std::function<std::unique_ptr<statement>(
+      ::gdsl::rreil::flop, std::unique_ptr<variable>,
+      std::unique_ptr<variable_limited>,
+      std::vector<std::unique_ptr<variable_limited>>)>;
+  using prim_ctor_t = std::function<std::unique_ptr<statement>(
+      std::string, std::vector<std::unique_ptr<variable_limited>>,
+      std::vector<std::unique_ptr<variable_limited>>)>;
+  using _throw_ctor_t = std::function<std::unique_ptr<statement>(
+      std::unique_ptr<::gdsl::rreil::exception>)>;
+  using arch_exception_ctor_t =
+      std::function<std::unique_ptr<::gdsl::rreil::exception>(std::string)>;
+  using shared_exception_ctor_t =
+      std::function<std::unique_ptr<::gdsl::rreil::exception>(
+          shared_exception_type)>;
+
+ private:
+  variable_ctor_t variable_ctor = NULL;
+  variable_limited_ctor_t variable_limited_ctor = NULL;
+  address_ctor_t address_ctor = NULL;
+  expr_cmp_ctor_t expr_cmp_ctor = NULL;
+  expr_binop_ctor_t expr_binop_ctor = NULL;
+  expr_ext_ctor_t expr_ext_ctor = NULL;
+  expr_sexpr_ctor_t expr_sexpr_ctor = NULL;
+  arch_id_ctor_t arch_id_ctor = NULL;
+  shared_id_ctor_t shared_id_ctor = NULL;
+  _virtual_ctor_t _virtual_ctor = NULL;
+  lin_binop_ctor_t lin_binop_ctor = NULL;
+  lin_imm_ctor_t lin_imm_ctor = NULL;
+  lin_scale_ctor_t lin_scale_ctor = NULL;
+  lin_var_ctor_t lin_var_ctor = NULL;
+  arbitrary_ctor_t arbitrary_ctor = NULL;
+  sexpr_cmp_ctor_t sexpr_cmp_ctor = NULL;
+  sexpr_lin_ctor_t sexpr_lin_ctor = NULL;
+  assign_ctor_t assign_ctor = NULL;
+  load_ctor_t load_ctor = NULL;
+  store_ctor_t store_ctor = NULL;
+  ite_ctor_t ite_ctor = NULL;
+  _while_ctor_t _while_ctor = NULL;
+  cbranch_ctor_t cbranch_ctor = NULL;
+  branch_ctor_t branch_ctor = NULL;
+  floating_ctor_t floating_ctor = NULL;
+  prim_ctor_t prim_ctor = NULL;
+  _throw_ctor_t _throw_ctor = NULL;
+  arch_exception_ctor_t arch_exception_ctor = NULL;
+  shared_exception_ctor_t shared_exception_ctor = NULL;
+
+ protected:
+  std::vector<std::unique_ptr<statement>> _statements;
+
+  std::unique_ptr<variable> _variable;
+  std::unique_ptr<variable_limited> _variable_limited;
+  std::unique_ptr<address> _address;
+  std::unique_ptr<expr_cmp> _expr_cmp;
+
+  std::unique_ptr<statement> _statement;
+  std::unique_ptr<sexpr> _sexpr;
+  std::unique_ptr<linear> _linear;
+  std::unique_ptr<id> _id;
+  std::unique_ptr<expr> _expr;
+  std::unique_ptr<gdsl::rreil::exception> _exception;
+
+ public:
+  std::unique_ptr<variable> retrieve_variable() { return std::move(_variable); }
+
+  std::unique_ptr<variable_limited> retrieve_variable_limited() {
+    return std::move(_variable_limited);
+  }
+
+  std::unique_ptr<address> retrieve_address() { return std::move(_address); }
+
+  std::unique_ptr<expr_cmp> retrieve_expr_cmp() { return std::move(_expr_cmp); }
+
+  std::unique_ptr<statement> retrieve_statement() {
+    return std::move(_statement);
+  }
+
+  std::vector<std::unique_ptr<statement>> retrieve_statements() {
+    return std::move(_statements);
+  }
+
+  std::unique_ptr<sexpr> retrieve_sexpr() { return std::move(_sexpr); }
+
+  std::unique_ptr<linear> retrieve_linear() { return std::move(_linear); }
+
+  std::unique_ptr<id> retrieve_id() { return std::move(_id); }
+
+  std::unique_ptr<expr> retrieve_expr() { return std::move(_expr); }
+
+  std::unique_ptr<::gdsl::rreil::exception> retrieve_exception() {
+    return std::move(_exception);
+  }
+
+  virtual void visit(variable const* v);
+  virtual void visit(variable_limited const* v);
+  virtual void visit(address const* v);
+  virtual void visit(expr_cmp const* v);
+
+  virtual void visit(assign const* a);
+  virtual void visit(load const* a);
+  virtual void visit(store const* a);
+  virtual void visit(ite const* a);
+  virtual void visit(_while const* a);
+  virtual void visit(cbranch const* a);
+  virtual void visit(branch const* a);
+  virtual void visit(floating const* a);
+  virtual void visit(prim const* a);
+  virtual void visit(_throw const* a);
+
+  virtual void visit(arbitrary const* a);
+  virtual void visit(sexpr_cmp const* a);
+  virtual void visit(sexpr_lin const* a);
+
+  virtual void visit(lin_binop const* a);
+  virtual void visit(lin_imm const* a);
+  virtual void visit(lin_scale const* a);
+  virtual void visit(lin_var const* a);
+
+  virtual void visit(arch_id const* a);
+  virtual void visit(shared_id const* a);
+  virtual void visit(_virtual const* a);
+
+  virtual void visit(expr_binop const* a);
+  virtual void visit(expr_ext const* a);
+  virtual void visit(expr_sexpr const* a);
+
+  virtual void visit(arch_exception const* a);
+  virtual void visit(shared_exception const* a);
+
+  void _(variable_ctor_t c) { this->variable_ctor = c; }
+  void _(variable_limited_ctor_t c) { this->variable_limited_ctor = c; }
+  void _(address_ctor_t c) { this->address_ctor = c; }
+  void _(expr_cmp_ctor_t c) { this->expr_cmp_ctor = c; }
+
+  void _(expr_binop_ctor_t c) { this->expr_binop_ctor = c; }
+  void _(expr_ext_ctor_t c) { this->expr_ext_ctor = c; }
+  void _(expr_sexpr_ctor_t c) { this->expr_sexpr_ctor = c; }
+
+  void _(arch_id_ctor_t c) { this->arch_id_ctor = c; }
+  void _(shared_id_ctor_t c) { this->shared_id_ctor = c; }
+  void _(_virtual_ctor_t c) { this->_virtual_ctor = c; }
+
+  void _(lin_binop_ctor_t c) { this->lin_binop_ctor = c; }
+  void _(lin_imm_ctor_t c) { this->lin_imm_ctor = c; }
+  void _(lin_scale_ctor_t c) { this->lin_scale_ctor = c; }
+  void _(lin_var_ctor_t c) { this->lin_var_ctor = c; }
+
+  void _(arbitrary_ctor_t c) { this->arbitrary_ctor = c; }
+  void _(sexpr_cmp_ctor_t c) { this->sexpr_cmp_ctor = c; }
+  void _(sexpr_lin_ctor_t c) { this->sexpr_lin_ctor = c; }
+
+  void _(assign_ctor_t c) { this->assign_ctor = c; }
+  void _(load_ctor_t c) { this->load_ctor = c; }
+  void _(store_ctor_t c) { this->store_ctor = c; }
+  void _(ite_ctor_t c) { this->ite_ctor = c; }
+  void _(_while_ctor_t c) { this->_while_ctor = c; }
+  void _(cbranch_ctor_t c) { this->cbranch_ctor = c; }
+  void _(branch_ctor_t c) { this->branch_ctor = c; }
+  void _(floating_ctor_t c) { this->floating_ctor = c; }
+  void _(prim_ctor_t c) { this->prim_ctor = c; }
+  void _(_throw_ctor_t c) { this->_throw_ctor = c; }
+  void _(arch_exception_ctor_t c) { this->arch_exception_ctor = c; }
+  void _(shared_exception_ctor_t c) { this->shared_exception_ctor = c; }
+};
+
+}  // namespace rreil
+}  // namespace gdsl
---
+++ libs/cppgdsl/src/rreil/copy_visitor.cpp
@@ -0,0 +1,309 @@
+#include <cppgdsl/rreil/copy_visitor.h>
+#include <cppgdsl/rreil/rreil.h>
+#include <cppgdsl/rreil/statement/floating.h>
+#include <vector>
+
+using namespace std;
+
+void gdsl::rreil::copy_visitor::visit(variable const *a) {
+  a->get_id().accept(*this);
+  auto id = std::move(_id);
+  if (variable_ctor != NULL)
+    _variable = variable_ctor(std::move(id), a->get_offset());
+  else
+    _variable = make_variable(std::move(id), a->get_offset());
+}
+
+void gdsl::rreil::copy_visitor::visit(variable_limited const *a) {
+  a->get_id().accept(*this);
+  auto id = std::move(_id);
+  if (variable_limited_ctor != NULL)
+    _variable_limited =
+        variable_limited_ctor(std::move(id), a->get_offset(), a->get_size());
+  else
+    _variable_limited =
+        make_variable(std::move(id), a->get_offset(), a->get_size());
+}
+
+void gdsl::rreil::copy_visitor::visit(address const *a) {
+  a->get_lin().accept(*this);
+  std::unique_ptr<linear> lin = std::move(_linear);
+  if (address_ctor != NULL)
+    _address = address_ctor(a->get_size(), std::move(lin));
+  else
+    _address = make_address(a->get_size(), lin);
+}
+
+void gdsl::rreil::copy_visitor::visit(expr_cmp const *a) {
+  a->get_lhs().accept(*this);
+  std::unique_ptr<linear> lhs = std::move(_linear);
+  a->get_rhs().accept(*this);
+  std::unique_ptr<linear> rhs = std::move(_linear);
+  if (expr_cmp_ctor != NULL)
+    _expr_cmp = expr_cmp_ctor(a->get_op(), std::move(lhs), std::move(rhs));
+  else
+    _expr_cmp = make_expr(a->get_op(), std::move(lhs), std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(_throw const *a) {
+  a->get_inner().accept(*this);
+  std::unique_ptr<gdsl::rreil::exception> e = std::move(_exception);
+  if (_throw_ctor != NULL)
+    _statement = _throw_ctor(std::move(e));
+  else
+    _statement = make_throw(std::move(e));
+}
+
+void gdsl::rreil::copy_visitor::visit(assign const *a) {
+  a->get_lhs().accept(*this);
+  std::unique_ptr<variable> lhs = std::move(_variable);
+  a->get_rhs().accept(*this);
+  std::unique_ptr<expr> rhs = std::move(_expr);
+  if (assign_ctor != NULL)
+    _statement = assign_ctor(a->get_size(), std::move(lhs), std::move(rhs));
+  else
+    _statement = make_assign(a->get_size(), std::move(lhs), std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(load const *a) {
+  a->get_lhs().accept(*this);
+  std::unique_ptr<variable> lhs = std::move(_variable);
+  a->get_address().accept(*this);
+  std::unique_ptr<address> address = std::move(_address);
+  if (load_ctor != NULL)
+    _statement = load_ctor(a->get_size(), std::move(lhs), std::move(address));
+  else
+    _statement = make_load(a->get_size(), std::move(lhs), std::move(address));
+}
+
+void gdsl::rreil::copy_visitor::visit(store const *a) {
+  a->get_address().accept(*this);
+  std::unique_ptr<address> address = std::move(this->_address);
+  a->get_rhs().accept(*this);
+  std::unique_ptr<linear> rhs = std::move(_linear);
+  if (store_ctor != NULL)
+    _statement = store_ctor(a->get_size(), std::move(address), std::move(rhs));
+  else
+    _statement = make_store(a->get_size(), std::move(address), std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(ite const *a) {
+  a->get_cond().accept(*this);
+  std::unique_ptr<sexpr> cond = std::move(this->_sexpr);
+  vector<std::unique_ptr<statement>> then_branch;
+  for (auto const &stmt : a->get_then_branch()) {
+    stmt.accept(*this);
+    then_branch.push_back(std::move(_statement));
+  }
+  vector<std::unique_ptr<statement>> else_branch;
+  for (auto const &stmt : a->get_else_branch()) {
+    stmt.accept(*this);
+    else_branch.push_back(std::move(_statement));
+  }
+  if (ite_ctor != NULL)
+    _statement = ite_ctor(std::move(cond), std::move(then_branch),
+                          std::move(else_branch));
+  else
+    _statement = make_ite(std::move(cond), std::move(then_branch),
+                          std::move(else_branch));
+}
+
+void gdsl::rreil::copy_visitor::visit(_while const *a) {
+  a->get_cond().accept(*this);
+  std::unique_ptr<sexpr> cond = std::move(_sexpr);
+  vector<std::unique_ptr<statement>> body;
+  for (auto const &stmt : a->get_body()) {
+    stmt.accept(*this);
+    body.push_back(std::move(_statement));
+  }
+  if (_while_ctor != NULL)
+    _statement = _while_ctor(std::move(cond), std::move(body));
+  else
+    _statement = make_while(std::move(cond), std::move(body));
+}
+
+void gdsl::rreil::copy_visitor::visit(cbranch const* a) {
+  a->get_cond().accept(*this);
+  std::unique_ptr<sexpr> cond = std::move(_sexpr);
+  a->get_target_true().accept(*this);
+  std::unique_ptr<address> target_true = std::move(_address);
+  a->get_target_false().accept(*this);
+  std::unique_ptr<address> target_false = std::move(_address);
+  if (cbranch_ctor != NULL)
+    _statement = cbranch_ctor(std::move(cond), std::move(target_true),
+                              std::move(target_false));
+  else
+    _statement = make_cbranch(std::move(cond), std::move(target_true),
+                              std::move(target_false));
+  ;
+}
+
+void gdsl::rreil::copy_visitor::visit(branch const *a) {
+  a->get_target().accept(*this);
+  std::unique_ptr<address> target = std::move(_address);
+  if (branch_ctor != NULL)
+    _statement = branch_ctor(std::move(target), a->get_hint());
+  else
+    _statement = make_branch(std::move(target), a->get_hint());
+}
+
+void gdsl::rreil::copy_visitor::visit(floating const *a) {
+  a->get_flags().accept(*this);
+  std::unique_ptr<variable> flags = std::move(_variable);
+  a->get_lhs().accept(*this);
+  std::unique_ptr<variable_limited> lhs = std::move(_variable_limited);
+  vector<std::unique_ptr<variable_limited>> rhs;
+  for (auto const &var : a->get_rhs()) {
+    var.accept(*this);
+    rhs.push_back(std::move(_variable_limited));
+  }
+  if (floating_ctor != NULL)
+    _statement = floating_ctor(a->get_op(), std::move(flags), std::move(lhs),
+                               std::move(rhs));
+  else
+    _statement = make_floating(a->get_op(), std::move(flags), std::move(lhs),
+                               std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(prim const *a) {
+  vector<std::unique_ptr<variable_limited>> lhs;
+  for (auto const &var : a->get_lhs()) {
+    var.accept(*this);
+    lhs.push_back(std::move(_variable_limited));
+  }
+  vector<std::unique_ptr<variable_limited>> rhs;
+  for (auto const &var : a->get_rhs()) {
+    var.accept(*this);
+    rhs.push_back(std::move(_variable_limited));
+  }
+  if (prim_ctor != NULL)
+    _statement = prim_ctor(a->get_op(), std::move(lhs), std::move(rhs));
+  else
+    _statement = make_prim(a->get_op(), std::move(lhs), std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(arbitrary const *a) {
+  if (arbitrary_ctor != NULL)
+    _sexpr = arbitrary_ctor();
+  else
+    _sexpr = make_arbitrary();
+}
+
+void gdsl::rreil::copy_visitor::visit(sexpr_cmp const *a) {
+  a->get_inner().accept(*this);
+  std::unique_ptr<expr_cmp> inner = std::move(_expr_cmp);
+  if (sexpr_cmp_ctor != NULL)
+    _sexpr = sexpr_cmp_ctor(a->get_size(), std::move(inner));
+  else
+    _sexpr = make_sexpr(a->get_size(), std::move(inner));
+}
+
+void gdsl::rreil::copy_visitor::visit(sexpr_lin const* a) {
+  a->get_lin().accept(*this);
+  std::unique_ptr<linear> lin = std::move(_linear);
+  if (sexpr_lin_ctor != NULL)
+    _sexpr = sexpr_lin_ctor(std::move(lin));
+  else
+    _sexpr = make_sexpr(std::move(lin));
+}
+
+void gdsl::rreil::copy_visitor::visit(lin_binop const* a) {
+  a->get_lhs().accept(*this);
+  std::unique_ptr<linear> lhs = std::move(_linear);
+  a->get_rhs().accept(*this);
+  std::unique_ptr<linear> rhs = std::move(_linear);
+  if (lin_binop_ctor != NULL)
+    _linear = lin_binop_ctor(a->get_op(), std::move(lhs), std::move(rhs));
+  else
+    _linear = make_linear(a->get_op(), std::move(lhs), std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(lin_imm const* a) {
+  if (lin_imm_ctor != NULL)
+    _linear = lin_imm_ctor(a->get_imm());
+  else
+    _linear = make_linear(a->get_imm());
+  ;
+}
+
+void gdsl::rreil::copy_visitor::visit(lin_scale const* a) {
+  a->get_operand().accept(*this);
+  std::unique_ptr<linear> opnd = std::move(_linear);
+  if (lin_scale_ctor != NULL)
+    _linear = lin_scale_ctor(a->get_const(), std::move(opnd));
+  else
+    _linear = make_linear(a->get_const(), std::move(opnd));
+}
+
+void gdsl::rreil::copy_visitor::visit(lin_var const* a) {
+  a->get_var().accept(*this);
+  std::unique_ptr<variable> var = std::move(_variable);
+  if (lin_var_ctor != NULL)
+    _linear = lin_var_ctor(std::move(var));
+  else
+    _linear = make_linear(std::move(var));
+}
+
+void gdsl::rreil::copy_visitor::visit(arch_id const* a) {
+  if (arch_id_ctor != NULL)
+    _id = arch_id_ctor(a->get_name());
+  else
+    _id = make_id(a->get_name());
+}
+
+void gdsl::rreil::copy_visitor::visit(shared_id const* a) {
+  if (shared_id_ctor != NULL)
+    _id = shared_id_ctor(a->get_inner());
+  else
+    _id = make_id(a->get_inner());
+}
+
+void gdsl::rreil::copy_visitor::visit(_virtual const* a) {
+  if (_virtual_ctor != NULL)
+    _id = _virtual_ctor(a->get_t(), a->get_opt());
+  else
+    _id = make_id(a->get_t(), a->get_opt());
+}
+
+void gdsl::rreil::copy_visitor::visit(expr_binop const* a) {
+  a->get_lhs().accept(*this);
+  std::unique_ptr<linear> lhs = std::move(_linear);
+  a->get_rhs().accept(*this);
+  std::unique_ptr<linear> rhs = std::move(_linear);
+  if (expr_binop_ctor != NULL)
+    _expr = expr_binop_ctor(a->get_op(), std::move(lhs), std::move(rhs));
+  else
+    _expr = make_expr(a->get_op(), std::move(lhs), std::move(rhs));
+}
+
+void gdsl::rreil::copy_visitor::visit(expr_ext const* a) {
+  a->get_operand().accept(*this);
+  std::unique_ptr<linear> opnd = std::move(_linear);
+  if (expr_ext_ctor != NULL)
+    _expr = expr_ext_ctor(a->get_op(), a->get_fromsize(), std::move(opnd));
+  else
+    _expr = make_expr(a->get_op(), a->get_fromsize(), std::move(opnd));
+}
+
+void gdsl::rreil::copy_visitor::visit(expr_sexpr const* a) {
+  a->get_inner().accept(*this);
+  std::unique_ptr<sexpr> inner = std::move(_sexpr);
+  if (expr_sexpr_ctor != NULL)
+    _expr = expr_sexpr_ctor(std::move(inner));
+  else
+    _expr = make_expr(std::move(inner));
+}
+
+void gdsl::rreil::copy_visitor::visit(arch_exception const* a) {
+  if (arch_exception_ctor != NULL)
+    _exception = arch_exception_ctor(a->get_name());
+  else
+    _exception = make_exception(a->get_name());
+}
+
+void gdsl::rreil::copy_visitor::visit(shared_exception const* a) {
+  if (shared_exception_ctor != NULL)
+    _exception = shared_exception_ctor(a->get_type());
+  else
+    _exception = make_exception(a->get_type());
+}
