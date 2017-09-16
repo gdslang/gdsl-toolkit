bd9979c731139cd5a6fba87ad36d655a59263772
Lovis J.I. Zenz <lovis.zenz@tum.de>
2017-09-16 10:58:47 +0200

    fix 0032-1

--- specifications/arm7/arm7.ml
+++ specifications/arm7/arm7.ml
@@ -2466,7 +2466,7 @@ end
 val combine-imm5 = do
   imm4 <- query $imm4;
   i <- query $i;
-  return (immediate (IMM5 (imm4^i)))
+  return (opnd-from-int (zx (imm4^i)))
 end
 
 # length
@@ -2505,7 +2505,7 @@ end
 val combine-opc2 = do
   opc12 <- query $opc12;
   opc22 <- query $opc22;
-  return (immediate (IMM4 (opc12^opc22)))
+  return (opnd-from-int (zx (opc12^opc22)))
 end
 
 # combine-u-opc2
@@ -2513,7 +2513,7 @@ val combine-u-opc2 = do
   u <- query $u;
   opc12 <- query $opc12;
   opc22 <- query $opc22;
-  return (immediate (IMM5 (u^opc12^opc22)))
+  return (opnd-from-int (zx (u^opc12^opc22)))
 end
 
 # opc13
