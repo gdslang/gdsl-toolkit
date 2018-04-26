cc5413b0edb4d7fd68a5fbd412c6c96382196eca
Lovis J.I. Zenz <lovis.zenz@tum.de>
2018-04-26 12:48:31 +0200

    fix 0299

--- specifications/arm7/arm7.ml
+++ specifications/arm7/arm7.ml
@@ -1261,8 +1261,8 @@ end
 ### VFPExpandImm [[A7.5.1]]
 val vfpexpandimm imm8 n = case imm8 of
     'i7:1 i6:1 i50:6': case n of
-        32: return (immediate (IMM32 (i7^(not i6)^i6^i6^i6^i6^i6^i50^'0000000000000000000')))
-      | 64: return (immediate (IMM64 (i7^(not i6)^i6^i6^i6^i6^i6^i6^i6^i6^i50^'000000000000000000000000000000000000000000000000')))
+        32: immediate (IMM32 (i7^(not i6)^i6^i6^i6^i6^i6^i50^'0000000000000000000'))
+      | 64: immediate (IMM64 (i7^(not i6)^i6^i6^i6^i6^i6^i6^i6^i6^i50^'000000000000000000000000000000000000000000000000'))
     end
 end
 # ----------------------------------------------------------------------
