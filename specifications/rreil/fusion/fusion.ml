f714340e93cbb05a551f02d84949770a68aa4d1e
Lovis J.I. Zenz <lovis.zenz@tum.de>
2018-05-28 14:49:10 +0200

    added val equal which compares two sem_sexpr, to fusion.ml

--- specifications/rreil/fusion/fusion.ml
+++ specifications/rreil/fusion/fusion.ml
@@ -37,7 +37,7 @@ end
 
 val get-fusable c fusable stmts = case stmts of
 	  SEM_CONS s : case s.hd of
-	  	  SEM_ITE t : get-fusable c (append fusable t) s.tl
+	  	  SEM_ITE t : if (equal t.cond c) then get-fusable c (append fusable t) s.tl else fusable
 		| _			: fusable
 	| SEM_NIL    : fusable
 end
@@ -45,7 +45,7 @@ end
 val fuse-bodies-ite-list head tail = case tail of
 	  SEM_CONS tt : case tt.hd of
 		SEM_ITE t : case head of
-			SEM_ITE h : fuse-bodies-ite-list (SEM_ITE {cond=t.cond,
+			SEM_ITE h : fuse-bodies-ite-list (SEM_ITE {cond=h.cond,
 													   then_branch=(append h.then_branch t.then_branch),
 													   else_branch=(append h.else_branch t.else_branch)}) tt.tl
 		end
@@ -67,4 +67,79 @@ val append a b = case a of
 			| SEM_NIL	 : SEM_CONS {hd=s.hd, tl=b}
 		| SEM_NIL	 : a
 	| SEM_NIL	 : b
+end
+
+val equal a b = case a of
+	  SEM_SEXPR_LIN l  : case l of
+	  	  SEM_LIN_VAR v : case v of
+		  	{id, off}: case b of
+				  SEM_SEXPR_LIN ll : case ll of
+				  	  SEM_LIN_VAR vv : case vv of
+					  	{idid, offoff} : case id of
+							  ### add additional sem_ids here
+							    FLOATING_FLAGS : case idid of
+									  FLOATING_FLAGS : (off == offoff)
+									| _				 : '0'
+								end
+							  | VIRT_T vt	   : case idid of
+							  		  VIRT_T vtvt : if vt == vtvt then (off == offoff) else '0'
+									| _			  : '0'
+							    end
+							  | VIRT_O vo	   : case idid of
+							  		  VIRT_O vovo : if vo == vovo then (off == offoff) else '0'
+									| _			  : '0'
+							    end
+						end
+					  end
+					| _				 : '0'
+				  end
+				| _				   : '0'
+			end
+		  end
+		| SEM_LIN_IMM i : case i of
+			{con} : case b of
+				  SEM_LIN_IMM ii : case ii of
+					{concon} : (con == concon)
+				  end
+				| _			   : '0'
+			end
+		  end
+	  end
+	| SEM_SEXPR_CMP cm : case cm of
+		{sz, comp} : case b of
+			  SEM_SEXPR_CMP cmcm : case cmcm of
+			  	{szsz, compcomp} : case comp of
+					  SEM_CMPEQ  : case compcomp of
+					  	  SEM_CMPEQ : (sz == szsz)
+						| _			: '0'
+					  end
+					| SEM_CMPNEQ : case compcomp of
+					  	  SEM_CMPNEQ : (sz == szsz)
+						| _			 : '0'
+					  end
+					| SEM_CMPLES : case compcomp of
+					  	  SEM_CMPLES : (sz == szsz)
+						| _			 : '0'
+					  end
+					| SEM_CMPLEU : case compcomp of
+					  	  SEM_CMPLEU : (sz == szsz)
+						| _			 : '0'
+					  end
+					| SEM_CMPLTS : case compcomp of
+					  	  SEM_CMPLTS : (sz == szsz)
+						| _			 : '0'
+					  end
+					| SEM_CMPLTU : case compcomp of
+					  	  SEM_CMPLTU : (sz == szsz)
+						| _			 : '0'
+					  end
+				end
+			  end
+			| _					 : '0'
+		end
+	  end
+	| SEM_SEXPR_ARB    : case b of
+		  SEM_SEXPR_ARB : '1'
+		| _				: '0'
+	  end
 end
